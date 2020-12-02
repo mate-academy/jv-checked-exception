d1918cef05dcf770b2c9af49e114b64ebdbffb30
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-02 18:14:55 +0200

    I hope it is solved

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+
+        if (password == null || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        } else {
+            return true;
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class UserService {
     public void registerUser(User user) {
 
+        PasswordValidator userPasswordValidator = new PasswordValidator();
+
+        try {
+            userPasswordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Wrong passwords");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was saved to data base!!!");
     }
 }
