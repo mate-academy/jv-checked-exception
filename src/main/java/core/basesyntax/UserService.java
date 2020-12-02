fb7e74e3b9269577b86ba399d01e7019f8d6e118
Resci <vIsUAl123>
2020-12-02 15:31:10 +0200

    Add solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
+    PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
 
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was saved to data base!!!");
     }
 }
