b88bf59934e0e2a32e3fd6290c8a73b0ee481589
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-03 15:01:00 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -9,6 +9,6 @@ public class PasswordValidator {
         if (password == null || password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
-            return true;
+        return true;
     }
 }
