850e13cd376488196062b84a7e74c832cc3414fd
Alekearl <alexandrhurko@gmail.com>
2020-12-04 00:20:39 +0200

    implemented methods registerUser() in UserService class, method validate() in PasswordValidator class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public static boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (password.equals(repeatPassword) && password.length() >= 10) {
+            return true;
+        }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
