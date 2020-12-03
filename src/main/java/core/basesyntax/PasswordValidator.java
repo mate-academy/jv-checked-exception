9f8efeaf2e1a41887212271c3e31c3938312792b
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-03 16:20:35 +0200

    methods validate() and registerUser()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password.equals(repeatPassword) && password.length() > 9) {
+            return true;
+        }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
