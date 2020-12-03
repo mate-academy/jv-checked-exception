2b24a828ee741f3015cb770bc6fa6dbc85f7420b
Likh-Alex <likhachov991@protonmail.com>
2020-12-03 18:13:58 +0200

    Removed empty lines

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-
         if (password != null && (password.equals(repeatPassword)
                 && password.length() >= 10)) {
             return true;
         }
-
         throw new PasswordValidationException("Wrong passwords");
     }
 }
