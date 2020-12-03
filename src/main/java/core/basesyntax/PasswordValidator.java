99aa65bfaa4da77695d0575a514faa73e26549ed
Tonitrus <grom4er@gmail.com>
2020-12-03 14:46:05 +0200

    Complete

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    private static final int MIN_LENGTH = 9;
+
+    public static boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password != null && password.equals(repeatPassword)
+                && password.length() > MIN_LENGTH) {
+            return true;
+        }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
