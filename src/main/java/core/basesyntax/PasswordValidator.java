78ac212909d4791876bf2488912fc27949cd7405
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-03 14:07:58 +0100

    mvn clean package BUILD SUCCESS

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
-            throw new PasswordValidationException("Wrong passwords");
+        if (password != null && password.equals(repeatPassword) && password.length() >= 10) {
+            return true;
         }
-        return true;
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
