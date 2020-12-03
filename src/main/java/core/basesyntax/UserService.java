59aaf052116344a8fd0c71d6a75a89fe9de61f8a
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-03 19:22:59 +0200

    Style fixed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         try {
-                new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
-                saveUser(user);
+            new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
