625c7e2df4269883e8635326c6d47b3b84baba6d
Vitalik Kopach <apple_air@ukr.net>
2020-12-03 20:36:40 +0200

    Changed classes UserService and PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,19 +4,11 @@ public class PasswordValidator {
 
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-
-        if (password == null | repeatPassword == null) {
+        if ((password == null | repeatPassword == null)
+                || (password.equals(repeatPassword) == false)
+                || (password.length() < 10 | repeatPassword.length() < 10)) {
             throw new PasswordValidationException("Wrong passwords");
         }
-
-        if (password.equals(repeatPassword) == false) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-
-        if (password.length() < 10 | repeatPassword.length() < 10) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-
         return true;
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,22 +3,17 @@ package core.basesyntax;
 public class UserService {
 
     public void registerUser(User user) {
-
         try {
-
             PasswordValidator validator = new PasswordValidator();
-            UserService userService = new UserService();
-
             if (validator.validate(user.getPassword(), user.getRepeatPassword())) {
-                userService.saveUser(user);
+                saveUser(user);
             } else {
                 throw new PasswordValidationException("Wrong passwords");
             }
-
+            validator.validate(user.getPassword(), user.getRepeatPassword());
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
-
     }
 
     public void saveUser(User user) {
