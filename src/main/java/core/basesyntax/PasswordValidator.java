47130d3321a18bf8f29f717cfc34f40effcfbbac
kerrender <agreed_note@hotmail.com>
2020-12-03 19:46:49 +0200

    if statement refactored

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,14 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public static boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (repeatPassword != null && repeatPassword.equals(password)) {
-            if (password.length() < 10) {
-                throw new PasswordValidationException("Wrong passwords");
-            } else {
-                return true;
-            }
-        } else {
-            throw new PasswordValidationException("Wrong passwords");
+        if (repeatPassword != null && repeatPassword.equals(password) && password.length() >= 10) {
+            return true;
         }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
