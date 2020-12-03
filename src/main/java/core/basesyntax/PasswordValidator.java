f4512e4a37a7c075c2f3bc06482039e7815f88bc
egorka <entertainment.veks@gmail.com>
2020-12-03 20:32:30 +0200

    yegor sereda 1st commit

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate (String password, String repeatPassword) throws PasswordValidationException {
+        if (password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        return true;
     }
 }
