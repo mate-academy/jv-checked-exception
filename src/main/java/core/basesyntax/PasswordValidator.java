8c6715e498f62189365d2fefa00836cc1cca5f5c
kerrender <agreed_note@hotmail.com>
2020-12-03 19:34:15 +0200

    redundant if statetments deleted

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public static boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null && repeatPassword == null) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-
-        if (repeatPassword.equals(password)) {
+        if (repeatPassword != null && repeatPassword.equals(password)) {
             if (password.length() < 10) {
                 throw new PasswordValidationException("Wrong passwords");
             } else {
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         try {
-            if (PasswordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
-                saveUser(user);
-            }
+            PasswordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");;
         }
-
     }
 
     public void saveUser(User user) {
