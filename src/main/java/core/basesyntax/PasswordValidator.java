02cb47227e4b78e7abe3962c7d54bba9aecedfe5
Vitalik Kopach <apple_air@ukr.net>
2020-12-03 18:33:25 +0200

    added class PasswordValidationException and changed classes UserService and PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+
+    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+
+        if (password == null | repeatPassword == null) {
+            throw new PasswordValidationException ("Wrong passwords");
+        }
+
+        if (password.equals(repeatPassword) == false) {
+            throw new PasswordValidationException ("Wrong passwords");
+        }
+
+        if (password.length() < 10 | repeatPassword.length() < 10) {
+            throw new PasswordValidationException ("Wrong passwords");
+        }
+
+        return true;
     }
 }
+
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,27 @@
 package core.basesyntax;
 
 public class UserService {
+
     public void registerUser(User user) {
+
+        try {
+
+            PasswordValidator validator = new PasswordValidator();
+            UserService userService = new UserService();
+
+            if(validator.validate(user.getPassword(), user.getRepeatPassword())){
+                userService.saveUser(user);
+            } else {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+
+        } catch (PasswordValidationException e){
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
 
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was saved to data base!!!");
     }
 }
