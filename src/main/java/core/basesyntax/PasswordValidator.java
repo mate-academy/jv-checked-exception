da1dbe51fc628ceed4905b3de6c06509e54a6ec5
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-03 15:01:12 +0200

    upgrade if conduction in password validation.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null
-                || password.length() < 10 || repeatPassword.length() < 10
-                || !password.equals(repeatPassword)) {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < 10 || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
         return true;
