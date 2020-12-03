73294102938f68c8c6141bc26b19560390ca845b
Tonitrus <grom4er@gmail.com>
2020-12-03 17:54:21 +0200

    change constant value

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    private static final int MIN_LENGTH = 9;
+    private static final int MIN_LENGTH = 10;
 
     public static boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password != null && password.equals(repeatPassword)
-                && password.length() > MIN_LENGTH) {
+                && password.length() >= MIN_LENGTH) {
             return true;
         }
         throw new PasswordValidationException("Wrong passwords");
