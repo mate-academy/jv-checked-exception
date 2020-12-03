3f12b35980cf3231d158ae3a95e061f132bfed76
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-03 15:44:44 +0200

    PasswordValidator was implemented

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
-public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
+import core.basesyntax.exceptions.PasswordValidationException;
 
+public class PasswordValidator {
+    public boolean validate(String password,
+                            String repeatPassword) throws PasswordValidationException {
 
         if(repeatPassword.length() >= 10 && password.length() >= 10) {
             return true;
         } else {
-
+            throw new PasswordValidationException("Wrong password");
         }
-
-        return false;
     }
 }
