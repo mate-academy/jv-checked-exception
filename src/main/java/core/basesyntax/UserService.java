7a07e462fac1f48314883292040b710e97075d35
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-04 11:44:33 +0200

    add correction

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,10 +4,11 @@ public class UserService {
     public void registerUser(User user) {
         PasswordValidator validator = new PasswordValidator();
         try {
-            if (!validator.validate(user.getPassword(), user.getRepeatPassword())) {
-                throw new PasswordValidationException();
+            if (validator.validate(user.getPassword(), user.getRepeatPassword())) {
+                saveUser(user);
+                return;
             }
-            saveUser(user);
+            throw new PasswordValidationException();
 
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
