67c552e2eba7bf05048aa9c153916dde5ca4e7a7
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-04 12:53:26 +0200

    first commit

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password.equals("")) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (repeatPassword.equals("")) {
+
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password.equals(repeatPassword) && password.length() >= 10) {
+
+            return true;
+        }
+
+        throw new PasswordValidationException("Wrong passwords");
+
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,27 @@
 package core.basesyntax;
 
 public class UserService {
+
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
 
+        if (user == null || passwordValidator == null) {
+
+            throw new NullPointerException("Not found user or passwordValidator");
+        }
+        try {
+            if (passwordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
+                saveUser(user);
+            }
+
+        } catch (PasswordValidationException e) {
+
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was saved to data base!!!");
     }
 }
