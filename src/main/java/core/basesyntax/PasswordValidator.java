540f8cad6cfa667623b208576e8e985c1aaa297c
Sashk52 <a.tsven@gmail.com>
2020-12-04 11:50:59 +0200

    made first soliction correction after mentor's rejecttion

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null || password.length() < 10
-                || repeatPassword.length() < 10 || !password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Wrong passwords");
-        } else {
+        if (password != null && password.length() >= 10
+                && repeatPassword.length() >= 10 && password.equals(repeatPassword)) {
             return true;
         }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
