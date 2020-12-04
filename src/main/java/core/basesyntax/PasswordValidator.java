fd79fbae9c9ffe139db0d774f491638dedbf6fd8
engineer027 <dp270597dma@gmail.com>
2020-12-04 11:43:58 +0200

    added solution

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password != null && repeatPassword != null
+        if (password != null
                 && password.equals(repeatPassword) && password.length() >= 10) {
             return true;
         }
