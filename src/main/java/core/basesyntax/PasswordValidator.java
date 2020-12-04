f652f426a142e85fc0573883097cfea1956cf507
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-04 10:56:10 +0200

    fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password != null && repeatPassword != null
-                && password.length() >= 10 && password.equals(repeatPassword)) {
+        if (password != null && password.length() >= 10 && password.equals(repeatPassword)) {
             return true;
-        } else {
-            throw new PasswordValidationException("Wrong passwords");
         }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
