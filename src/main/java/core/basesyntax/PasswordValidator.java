abcaf7fed25566b19a73c1d7ebc2c41f3bbf2381
Volodymyr Mazuryk <mzrkvolodymyr@gmail.com>
2020-12-04 10:34:23 +0200

    fixed small bugs;

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public static boolean validate(String password, String repeatPassword)
+    public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password != null && password.equals(repeatPassword) && password.length() >= 10) {
             return true;
