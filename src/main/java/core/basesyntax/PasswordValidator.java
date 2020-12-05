c66f12e122b41f95d9ac9e93f7b244850c457121
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-05 21:43:32 +0200

    first commit

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword) {
-        return false;
+        boolean isValidated = password.length() >= 10 && password.equals(repeatPassword)
+                && password != null;
+        if (!isValidated) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        return true;
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,10 +2,16 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (RuntimeException s) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was saved to data base!!!");
     }
 }
