9b0a8ec969e16aaece3b11ffaf62e45d7aafe814
Tetiana Chupika <mtv1301@gmail.com>
2021-02-16 20:00:05 +0200

    refactor solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,12 +5,9 @@ import core.basesyntax.exception.PasswordValidationException;
 public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-        if (user.getPassword().length() >= 10 && user.getRepeatPassword().length() >= 10
-                && user.getPassword().equals(user.getRepeatPassword())) {
-            saveUser(user);
-        }
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
