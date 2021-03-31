3497c07807b28ea2b89eb52d8de4ceb43d35af34
IhorRome <ihor.rome@gmail.com>
2021-03-31 13:25:18 +0300

    fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,6 @@ public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null
-                || password.length() == 0
                 || !password.equals(repeatPassword)
                 || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
