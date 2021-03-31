c7e03af64f095ab86b7078c3e587a18caa06cc1f
Vlad-Andr <andreev.v99@gmail.com>
2021-03-31 21:49:48 +0300

    added password validator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        boolean result = true;
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (password.length() < 10 || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        return result;
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class UserService {
     public void registerUser(User user) {
-
+        PasswordValidator pv = new PasswordValidator();
+        try {
+            pv.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+            System.exit(0);
+        }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
