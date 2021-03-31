74b8bf9c44e1c00d837c2a5e512c8808e9b745d2
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-03-31 06:15:28 +0300

    first solution, maven and UserServiceTest checks passed.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (repeatPassword == null || password == null || password.length() == 0) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        return true;
     }
 }
