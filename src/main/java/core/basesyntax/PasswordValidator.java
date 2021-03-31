98f88ccb3cb9ab216cdf05b063b37f86c35fd999
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-03-31 11:51:55 +0300

    combined ifs into one if

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,15 +5,11 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (repeatPassword == null || password == null || password.length() == 0) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-
-        if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-
-        if (password.length() < 10) {
+        if (repeatPassword == null
+                || password == null
+                || password.length() == 0
+                || password.length() < 10
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
 
