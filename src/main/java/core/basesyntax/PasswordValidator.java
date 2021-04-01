81067fddbcb43fc13bd7d945bec3496dbc2d87a0
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-04-01 18:31:39 +0200

    Created exception, its handling and modified method registerUser().

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password != null && repeatPassword != null
+                && password.equals(repeatPassword) && password.length() >= 10) {
+            return true;
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
+
