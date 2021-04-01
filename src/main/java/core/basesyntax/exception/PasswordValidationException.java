88ece030df4c6aa1b63adc7cb2f3afb4b1d83712
Koziarevskyi <koziarevskyi@gmail.com>
2021-04-01 22:42:00 +0300

    C002 - All Done, but java.lang.AssertionError: You should create a checked exception 'PasswordValidationException'.

---
+++ src/main/java/core/basesyntax/exception/PasswordValidationException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class PasswordValidationException extends RuntimeException {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
