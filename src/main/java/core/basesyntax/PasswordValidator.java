1c0c9b3f6d657ea6881555a545c65d5aa1a3628a
Elia2909 <290900rR>
2021-04-01 18:23:15 +0300

    Task password validator is done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null
+                || !password.equals(repeatPassword)
+                || password.length() < 8) {
+            throw new PasswordValidationException("Wrong passwords");
+
+        }
+        return true;
     }
+
 }
+
