154d5c55fc1aee9c9b31245bb8bd06cb9e45c3df
unknown <vovazalli@gmail.com>
2021-04-01 22:08:42 +0300

    Password validator successfull build

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+                                throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (password.equals(repeatPassword) && password.length() >= 10) {
+            return true;
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
