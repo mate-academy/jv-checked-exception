e51eb3c8ea86de711f655290f8910b9d25171485
Дмитро <dima4994@gmail.com>
2021-04-01 19:42:29 +0300

    There were implemented methods: - `registerUser()` in UserService class - `validate()` in PasswordValidator class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null
+                || !password.equals(repeatPassword)
+                || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        return true;
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class UserService {
     public void registerUser(User user) {
+        PasswordValidator passwordValidator = new PasswordValidator();
 
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException | NullPointerException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
