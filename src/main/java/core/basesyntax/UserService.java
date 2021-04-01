0313eff8d936e333f12c0eb1478ebf519c808223
Margarita Kislukhina <marharita.kislukhina@gmail.com>
2021-04-01 18:48:08 +0200

    PasswordValidationException created, PasswordValidator created

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator passwordValidator = new PasswordValidator();
 
     public void registerUser(User user) {
         try {
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-            saveUser(user);
+            PasswordValidator passwordValidator = new PasswordValidator();
+            if (passwordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
+                saveUser(user);
+            }
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
