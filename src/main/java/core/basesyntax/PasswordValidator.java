59cfe3d81423aa5c9b1acdc6071e0273f492e8f0
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-04-02 00:47:59 +0300

    first commit, not ended task

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+                    throws PasswordValidationException {
+        if (repeatPassword.equals(password) && password.length() >= 10
+            && !password.equals(null) || !repeatPassword.equals(null)) {
+            return true;
+        }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
