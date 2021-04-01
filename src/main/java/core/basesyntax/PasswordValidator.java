61732480187c1ebb875a8a7d07b8f85b9e48d29f
peps <pepsithedoge@gmail.com>
2021-04-01 23:59:17 +0300

    wrapped lines

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,11 +6,13 @@ public class PasswordValidator {
 
     public static final int MINIMUM_PASSWORD_LENGTH = 10;
 
-    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password.length() < MINIMUM_PASSWORD_LENGTH || repeatPassword.length() < MINIMUM_PASSWORD_LENGTH) {
+        if (password.length() < MINIMUM_PASSWORD_LENGTH
+                || repeatPassword.length() < MINIMUM_PASSWORD_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
         }
         if (!password.equals(repeatPassword)) {
