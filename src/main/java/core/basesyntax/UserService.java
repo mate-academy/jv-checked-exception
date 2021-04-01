b020553bf61c10eaa1f942f6547875030c7855f5
anufriiev <dimaanufriev1012@gmail.com>
2021-04-01 20:21:08 +0300

    HW-4.PasswordValidation

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class UserService {
     public void registerUser(User user) {
-
+        PasswordValidator passwordValidator = new PasswordValidator();
+        if (passwordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
+            saveUser(user);
+            System.out.println("User saved");
+            return;
+        } else {
+        }
+        throw new PasswordValidationException("Your passwords are incorrect. Try again");
     }
 
     public void saveUser(User user) {
+
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
