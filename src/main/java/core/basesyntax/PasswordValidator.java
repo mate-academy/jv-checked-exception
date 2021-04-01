b0213f9cf3934db495ad77ec97b398843f284ea9
the5tranger <mishaantonyuk00@gmail.com>
2021-04-01 16:01:31 +0300

    HW-4 Exceptions v1.0

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,7 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() < 10 && repeatPassword.length() < 10) {
+        if (password.length() >= 10 && repeatPassword.length() >= 10) {
             return true;
         } else {
             throw new PasswordValidationException("Wrong passwords");
