f57e3f93f07e8330267c6a5b8358d18d126f6765
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-02 10:45:17 +0300

    remove redundant empty line

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,6 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password,
                             String repeatPassword) throws PasswordValidationException {
-
         if (password != null && password.equals(repeatPassword) && password.length() >= 10) {
             return true;
         }
