0a97dc63ce81be5ca47e1fecdffbe8946d510844
redxp <redxpertus1@gmail.com>
2021-04-02 08:50:44 +0300

    comment is changed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserService();
-        User user = new User("reastador@gmail.com","qwerty1111","qwerty1111");
+        User user = new User("reastador@gmail.com","qwerty111","qwerty1111");
         userService.registerUser(user);
     }
 }
--- src/main/java/core/basesyntax/exception/PasswordValidationException.java
+++ src/main/java/core/basesyntax/exception/PasswordValidationException.java
@@ -1,10 +1,8 @@
 package core.basesyntax.exception;
 
 public class PasswordValidationException extends Exception {
-    private String message;
 
     public PasswordValidationException(String message) {
         super(message);
-        this.message = message;
     }
 }
