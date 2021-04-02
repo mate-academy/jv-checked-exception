8208aba526df3829897d65193a5a27f38d1b836d
Dalai <semenov.dalay@gmail.com>
2021-04-02 09:23:26 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import core.basesyntax.exception.PasswordValidationException;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password != null && password.equals(repeatPassword) && password.length() >= 10) {
             return true;
         }
