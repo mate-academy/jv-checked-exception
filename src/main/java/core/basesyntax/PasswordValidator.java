66a81999123311e91fe86ca0cb1240ff5afb56ef
Darina <to.darina.to>
2021-04-02 10:10:00 +0300

    review change1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,10 +5,8 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Wrong passwords");
-        } else if (!(password.length() >= 10 && repeatPassword.length() >= 10
-                && password.equals(repeatPassword))) {
+        if (!(password.length() >= 10 && repeatPassword.length() >= 10
+                && password.equals(repeatPassword)) && password == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
         return true;
