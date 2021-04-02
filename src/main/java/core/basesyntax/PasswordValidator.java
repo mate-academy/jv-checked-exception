7b4bcdf55c92c597ded8962c004668cf3719bb3a
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-04-02 09:37:24 +0300

    deleted redundant empty line

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,6 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-
         if (password == null || password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
