80dea6532d8faca2700c4d580fea6da5d52b3435
roman <nashpatsa@gmai.com>
2021-04-02 10:06:02 +0300

    redundanted empty line

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,14 +3,12 @@ package core.basesyntax;
 import core.basesyntax.exception.PasswordValidationException;
 
 public class PasswordValidator {
-
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password != null && password.equals(repeatPassword)
                 && password.length() >= 10) {
             return true;
         }
-
         throw new PasswordValidationException("Wrong passwords");
     }
 }
