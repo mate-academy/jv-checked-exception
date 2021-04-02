e2075173530009c2aeb720859bc14c7ef8a654b3
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-04-02 11:31:44 +0300

    resolve all mistakes

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,10 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords or length less than 10");
-        } else if (password.equals(repeatPassword) && password.length() >= 10) {
-            return true;
         } else {
-            throw new PasswordValidationException("Wrong passwords or length less than 10");
+            return true;
         }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,12 +4,10 @@ import core.basesyntax.exception.PasswordValidationException;
 
 public class UserService {
     public void registerUser(User user) {
-
         try {
             PasswordValidator passwordValidator = new PasswordValidator();
-            if (passwordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
-                saveUser(user);
-            }
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
