6e8383027742776355dd7742876da71b5e528a84
Oksana Lashchenko <lashchenkooksana@gmail.com>
2021-04-02 09:56:52 +0300

    all files added

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.PasswordValidationException;
+
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        boolean isValidated = password.equals(repeatPassword) && password.length() >= 10;
+        if (!isValidated) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        return true;
     }
 }
