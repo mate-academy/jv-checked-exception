55e561adb43de6d44c0906cd99b7236f8ccdc9bf
unknown <vovazalli@gmail.com>
2021-04-02 09:57:53 +0300

    simplified construction

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,11 @@ import core.basesyntax.exception.PasswordValidationException;
 public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
+        if ((password == null || repeatPassword == null)
+                || !password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
-        } else if (password.equals(repeatPassword) && password.length() >= 10) {
-            return true;
         } else {
-            throw new PasswordValidationException("Wrong passwords");
+            return true;
         }
     }
 }
