9d7e1573d78a13d432dd457cf60ebbd48cc7c6a0
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-04-02 10:46:45 +0200

    Made another requested changes.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,8 @@ public class PasswordValidator {
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password != null
-                && password.equals(repeatPassword) && password.length() >= 10) {
+                && password.equals(repeatPassword)
+                && password.length() >= 10) {
             return true;
         }
         throw new PasswordValidationException("Wrong passwords");
