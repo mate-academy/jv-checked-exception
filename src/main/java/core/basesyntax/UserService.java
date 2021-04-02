6999dff55b704986ee404d90be8b861e937b986a
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-02 22:17:52 +0300

    PasswordValidationException.java: fix format issue; PasswordValidator.java: added a check for null; UserService.java: fixed "try" logic. Ready for review.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,21 +1,18 @@
 package core.basesyntax;
-/*
-Метод registerUser () должен вызывать метод validate () и обрабатывать исключение.
-Если все в порядке, сохраните пользователя в базе данных (используйте метод saveUser ()), если нет,
-выведите сообщение «Ваши пароли неверны. Повторите попытку.».
- */
 
 import core.basesyntax.exception.PasswordValidationException;
 
 public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-        try{
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        try {
+            if (passwordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
+                saveUser(user);
+            }
+
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
-        saveUser(user);
     }
 
     public void saveUser(User user) {
--- src/main/java/core/basesyntax/exception/PasswordValidationException.java
+++ src/main/java/core/basesyntax/exception/PasswordValidationException.java
@@ -1,6 +1,6 @@
 package core.basesyntax.exception;
 
-public class PasswordValidationException extends Exception{
+public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
