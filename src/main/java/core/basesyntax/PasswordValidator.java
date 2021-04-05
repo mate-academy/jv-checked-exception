ccdc62901f447175cbdee29fe4528a8426f33f19
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-05 13:47:55 +0300

    used another condition.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,17 +3,13 @@ package core.basesyntax;
 import core.basesyntax.exception.PasswordValidationException;
 
 public class PasswordValidator {
-    private static final String MESSAGE_PASSWORD_HAS_NULL = "password or repeatPassword is null";
     private static final String MESSAGE_PASSWORDS_ARE_WRONG = "Wrong passwords";
 
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException(MESSAGE_PASSWORD_HAS_NULL);
+        if (password != null && password.equals(repeatPassword) && password.length() >= 10) {
+            return true;
         }
-        if (!(password.equals(repeatPassword) && password.length() >= 10)) {
-            throw new PasswordValidationException(MESSAGE_PASSWORDS_ARE_WRONG);
-        }
-        return true;
+        throw new PasswordValidationException(MESSAGE_PASSWORDS_ARE_WRONG);
     }
 }
