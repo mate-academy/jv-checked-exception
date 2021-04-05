2fa4bc2b29613da574cbff0a9005bb68d9c2ffd1
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-05 11:46:12 +0300

    Null check condition moved up and throw PasswordValidationException.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,16 @@ package core.basesyntax;
 import core.basesyntax.exception.PasswordValidationException;
 
 public class PasswordValidator {
+    private static final String MESSAGE_PASSWORD_HAS_NULL = "password or repeatPassword is null";
+    private static final String MESSAGE_PASSWORDS_ARE_WRONG = "Wrong passwords";
+
     public boolean validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if ((password == null || repeatPassword == null)
-                || !(password.equals(repeatPassword) && password.length() >= 10)) {
-            throw new PasswordValidationException("Wrong passwords");
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException(MESSAGE_PASSWORD_HAS_NULL);
+        }
+        if (!(password.equals(repeatPassword) && password.length() >= 10)) {
+            throw new PasswordValidationException(MESSAGE_PASSWORDS_ARE_WRONG);
         }
         return true;
     }
