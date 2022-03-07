275d67771f3ab20a9bdb21c13f2b5249f893fa9d
mykyta_arkhanhelskyi <nickarchangel97@gmail.com>
2022-03-07 14:20:56 +0200

    update task for migration to automatically approved task on github

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) {
-        return false;
+    public void validate(String password, String repeatPassword) {
+        //write your code here
     }
 }
+
