1ff834fe70da8664db9858bbe4088d6dc182e05a
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-03-20 14:48:33 +0200

    Implemented two method registerUser and validate. Created own checked exception.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (repeatPassword.length() < 10) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
+
+        return true;
     }
 }
