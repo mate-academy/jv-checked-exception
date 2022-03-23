1a26b321998d21fe5276f298428dbf4915ffc6dc
Anastasia <anastasian817@gmail.com>
2022-03-23 09:54:00 +0200

    added new classes

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
-    }
-
-    public void saveUser(User user){
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+    public void registerUser(User user) {
+        try {
+            new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
+            System.out.println("User " + user + " was saved to database!!!");
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 }
