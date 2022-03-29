e5af67c25e0a46582e8939eb624a870baea8c8c1
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-03-29 16:55:39 +0300

    hw has been done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) throws PasswordValidationException {
-        User user = new User("email@gmail.com", "password12", "password1" );
+        User user = new User("email@gmail.com", "password12", "password1");
         PasswordValidator passwordValidator = new PasswordValidator();
         passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
         UserService userService = new UserService();
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password.equals(repeatPassword) && password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Wrong passwords");
+            throw new NullPointerException("NullPointerException");
         }
         if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
