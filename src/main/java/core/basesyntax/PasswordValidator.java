1624e3ef72983bf929c8c76edd6d344d09d53d8c
Andrii Fabersky <Andriifabersky@gmail.com>
2022-03-29 14:37:29 +0300

    i did my homework

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int CHECK_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null) {
+            throw new PasswordValidationException("The password field is empty");
+        }
+
+        if (repeatPassword.length() < CHECK_LENGTH) {
+            throw new PasswordValidationException("Password less than ten");
+        }
+
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
