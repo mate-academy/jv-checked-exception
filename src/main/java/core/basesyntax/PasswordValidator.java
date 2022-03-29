066ef617701030bc8c3cf3a3c0d520736655dfff
Artur <zamelatskov@gmail.com>
2022-03-29 13:04:43 +0300

    adde PasswordValidationException and implementation UserService and PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
-public class PasswordValidator {
+public class PasswordValidator extends RuntimeException {
     public void validate(String password, String repeatPassword) {
-        //write your code here
+        if ((password.equals(repeatPassword) && password.length() >= 10) == false) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public void registerUser(User user) {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
