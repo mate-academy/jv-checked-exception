09898950ad72ea3a925550c9f5aae7d51f684d99
Artur <zamelatskov@gmail.com>
2022-03-29 15:45:23 +0300

    used clean package

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_PASSWORD = 10;
 
-    public void validate(String password, String repeatPassword) throws  PasswordValidationException{
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
         if (password == null || repeatPassword == null || !password.equals(repeatPassword)
                 || password.length() < MIN_PASSWORD) {
             throw new PasswordValidationException("Wrong passwords");
