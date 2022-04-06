2efe5189fe3786477c888ef37f52caf0ae3837e3
Vladyslav Chyver <vladyslav.chyver@gmail.com>
2022-04-06 21:08:11 +0300

    implementation was added

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public void registerUser(User user) {
+        try {
+            new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException exception) {
+            System.out.println("Your passwords are incorrect. Try again.");
+            return;
+        }
+        saveUser(user);
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
