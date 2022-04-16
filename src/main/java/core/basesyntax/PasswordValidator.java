b79f6df54174e82ba0e3189e6e8d0b446122c413
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-04-16 17:05:12 +0300

    done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        //write your code here
+        try {
+            throw new PasswordValidationException("Wrong passwords");
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public static final PasswordValidator passwordValidator = new PasswordValidator();
+
+    public void registerUser(User user) {
+        if (user.getPassword().length() >= 10
+                && user.getPassword().equals(user.getRepeatPassword())) {
+            saveUser(user);
+        } else {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        }
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
