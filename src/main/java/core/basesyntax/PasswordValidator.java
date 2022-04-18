c9802745e397c8e5990e1e2c42855368374d251b
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-18 19:50:55 +0300

    fixed checkstyle problems and test errors

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,12 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-         if (password == null
-               || repeatPassword == null
-               || !password.equals(repeatPassword)
-               || password.length() < MIN_LENGTH) {
+        if (password == null
+                || repeatPassword == null
+                || !password.equals(repeatPassword)
+                 || password.length() < MIN_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
-         }
+        }
     }
 }
 
