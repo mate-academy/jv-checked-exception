7e09c7aabe818c905efd00de2c533166ae82af6c
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-18 19:49:01 +0300

    fixed checkstyle problems and test errors

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -8,12 +8,12 @@ public class UserService {
     }
 
     public void registerUser(User user) {
-         try {
-                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-         }
-         catch (PasswordValidationException e) {
+        }
+        catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
-         }
+        }
     }
 }
