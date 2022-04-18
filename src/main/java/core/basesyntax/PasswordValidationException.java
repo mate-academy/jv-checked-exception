532fd6aed09dc4bc53a845142775882705fea96d
mitresko <mitresko12@gmail.com>
2022-04-18 18:29:43 +0300

    implement validate method

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -3,4 +3,7 @@ package core.basesyntax;
 //write your code here
 public class PasswordValidationException extends Exception {
 
+    public PasswordValidationException(String message) {
+        super(message);
+    }
 }
