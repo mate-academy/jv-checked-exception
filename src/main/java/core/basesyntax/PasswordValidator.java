c7775b55efbdd7a838462be00f87f8770e1a29fd
Igor <ZIM20093@gmail.com>
2022-05-01 17:08:34 +0300

    fixed validate(), registerUser()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() >= 10 && repeatPassword.length() >= 10
-                && password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Wrong passwords");
+        if (password != null && repeatPassword != null && password.length() >= 10
+                && repeatPassword.length() >= 10 && password.equals(repeatPassword)) {
+            return;
         }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
