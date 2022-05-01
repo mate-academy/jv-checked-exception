03f1b8899fc5481e5cd1c4820196e38f7b28e896
Maksym Bilan <golosei@gmail.com>
2022-05-01 11:01:32 +0300

    5th commit formatted lines

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null && password.length() == 0 || repeatPassword.length() == 0) {
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        if (password == null || repeatPassword == null && password.length() == 0
+                || repeatPassword.length() == 0) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password.equals(repeatPassword) && (password.length() < 10) || (!(password.equals(repeatPassword)))) {
+        if (password.equals(repeatPassword) && (password.length() < 10)
+                || (!(password.equals(repeatPassword)))) {
             throw new PasswordValidationException("Wrong passwords");
 
         }
