1c6341bebf2b608d03d56e3fc20c641c206fdece
Benatti <ssunica0@gmail.com>
2022-05-06 23:56:35 +0300

    commit

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.io.IOException;
 
 public class PasswordValidator {
-    static String chars = "\\\'\"@";
+    private static String chars = "\\\'\"@";
+    
     public static void validate(String password, String repeatPassword) throws IOException {
         if (!(password.equals(repeatPassword) && password.length() >= 10)) {
             throw new PasswordValidationException();
