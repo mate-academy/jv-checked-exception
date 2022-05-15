c0becbe022a4d801b28f13d75bbce390d0cf20dc
Viktoriia Haraniuk <haranyuk71723@gmail.com>
2022-05-15 15:58:54 +0300

    implemented registerUser and validate methods; created PasswordValidationException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int MINIMUN_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new RuntimeException();
+        }
+        if (!password.equals(repeatPassword) || password.length() < MINIMUN_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
