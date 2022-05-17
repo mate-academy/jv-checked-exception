1d358a6f12756bb9742af80f16c77d20d2311ec6
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-05-17 16:04:09 +0300

    BUILD SUCCESS

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int PASSWORD_LENGTH = 10;
     private static final String MESSAGE = "Wrong passwords";
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null || !password.equals(repeatPassword)
                 || password.length() < PASSWORD_LENGTH) {
             throw new PasswordValidationException(MESSAGE);
