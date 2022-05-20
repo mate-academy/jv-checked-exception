1bd123bc1b18ccaccefe1a0bfa6aaa2e26792e57
Алена <258258>
2022-05-20 17:29:02 +0300

    Added methods: - registerUser() in UserService class - validate() in PasswordValidator class - implements PasswordValidationException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
-
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
+
         if (password == null || password.length() < 10 || repeatPassword == null
-        || repeatPassword.length() < 10 || !password.equals(repeatPassword)){
+                || repeatPassword.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,19 +1,17 @@
 package core.basesyntax;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
-
 public class UserService {
-    public void registerUser(User user){
+    public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        }   catch (PasswordValidationException e) {
+        } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
