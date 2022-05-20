c35849ef6cca3d7c7914262e84cb873cad456ad7
Алена <258258>
2022-05-20 17:16:32 +0300

    Added methods: - registerUser() in UserService class - validate() in PasswordValidator class - implements PasswordValidationException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || password.length() < 10 || repeatPassword == null
+        || repeatPassword.length() < 10 || !password.equals(repeatPassword)){
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
