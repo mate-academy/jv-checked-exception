47a1ef8d8ef07a2b1e19b78549fc7f1c2b5a989b
olyaklipa <olyaklipa@gmail.com>
2022-05-22 02:07:07 +0300

    registerUser() and validate() methods are implemented

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final String MESSAGE = "Wrong passwords";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException(MESSAGE);
+        }
+        if (password.equals(repeatPassword)) {
+            if (password.length() < 10) {
+                throw new PasswordValidationException(MESSAGE);
+            }
+        } else {
+            throw new PasswordValidationException(MESSAGE);
+        }
     }
 }
