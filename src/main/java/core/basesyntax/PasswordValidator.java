39ad205b17074d158814d8a6ed5add8b72e72582
Ilya <zimmboco@gmail.com>
2022-05-23 19:12:51 +0300

    hw-4

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,10 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() < 10) {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Please, enter correct password!!");
+        }
+        if (!password.equals(repeatPassword) || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
