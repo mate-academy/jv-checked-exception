93131b9374e220866077fccd49afa632b5ce0975
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2022-05-27 14:46:54 +0300

    jv-checked-exception solution

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
+//        if (password == null || repeatPassword == null) {
+//            throw new PasswordValidationException("Wrong passwords");
+//        }
+        final int PASS_LENGTH = 10;
+        final boolean PASS_NOT_VALID = password == null
+                || repeatPassword == null
+                || password.length() < PASS_LENGTH
+                || repeatPassword.length() < PASS_LENGTH
+                || !(password.equals(repeatPassword));
+
+        if (PASS_NOT_VALID) {
+           throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
