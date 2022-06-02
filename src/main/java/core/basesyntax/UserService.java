29cfdf09455054c169389a5ad8206fcc5568423e
Anton Hut <tonnyhut@icloud.com>
2022-06-02 15:11:58 +0300

    Implemented method which handled an exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
+import java.sql.SQLOutput;
+
 public class UserService {
     public void registerUser(User user){
-        //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again." + e);
+        }
+
     }
 
     public void saveUser(User user){
