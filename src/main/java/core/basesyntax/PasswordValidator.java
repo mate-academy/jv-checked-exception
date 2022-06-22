8660373486464cbcdcdb5a23e1eea16ea1d62a96
Mykola Kutsyi <kytsij@gmail.com>
2022-06-22 17:46:47 +0300

    Task Password Validator complete. For checking.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int MINIMUM_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new RuntimeException();
+        }
+        if (!password.equals(repeatPassword) || password.length() < MINIMUM_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
