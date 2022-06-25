3b2595d0d1e8122e9159d7594050e848cd135641
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-25 05:39:05 +0300

    Refactored

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
+    public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-        try{
+        try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
@@ -12,7 +12,7 @@ public class UserService {
         saveUser(user);
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
