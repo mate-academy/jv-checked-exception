6425ed976a382036bad7a3dbd0197f7a29b608cc
Vasyl Piznak <piznak3145@gmail.com>
2022-06-26 14:24:45 +0300

    Build success.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,7 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null
-                || password.equals(repeatPassword) 
+                || password.equals(repeatPassword) == false
                 || password.length() < 10
                 || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
