350a94b050442d9c4e12743493e3620eabf202b9
Valerii-Sh <val-sh@ukr.net>
2022-07-01 23:26:30 +0300

    changed after review (2)

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    static int passwordLength = 10;
+    public static final int PASSWORD_LENGTH = 10;
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
 
         boolean notValidPassword = password == null
                 || repeatPassword == null
-                || password.length() < passwordLength
-                || repeatPassword.length() < passwordLength
+                || password.length() < PASSWORD_LENGTH
+                || repeatPassword.length() < PASSWORD_LENGTH
                 || !(password.equals(repeatPassword));
 
         if (notValidPassword) {
