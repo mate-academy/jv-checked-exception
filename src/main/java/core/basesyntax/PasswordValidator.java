8d659b9bc218686b95dc388c120cdfbe9bd271b1
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-03 07:49:54 +0300

    jv-checked-exception-hw5-done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+//import java.util.regex.PatternSyntaxException;
+
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    static final int MIN_PASSWORD = 10;
+    static final String EXEPTION_MESSAGE1 = "Wrong passwords";
+    static final String EXEPTION_MESSAGE2 = "Your passwords are incorrect. Try again.";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null) {
+            throw new PasswordValidationException(EXEPTION_MESSAGE1);
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException(EXEPTION_MESSAGE1);
+        }
+        if (password.length() < MIN_PASSWORD) {
+            throw new PasswordValidationException(EXEPTION_MESSAGE2);
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
+
+    public void registerUser(User user) {
         //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            if (user.getPassword() != null || user.getRepeatPassword() != null) {
+                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+                saveUser(user);
+            }
+        } catch (PasswordValidationException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
