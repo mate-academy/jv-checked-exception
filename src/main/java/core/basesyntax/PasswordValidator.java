52f0627a99e00cf7183ffacd241946d3ac64a8be
Misha Getsko <getsko.gil@gmail.com>
2022-07-04 12:25:48 +0300

    I have implemented registerUser() and validate() method. Created a custom CHECKED exception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+
+    private static final int PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        boolean checkedDIfferent = false;
+        boolean checkedLength = false;
+        if (password != null || repeatPassword != null) {
+            checkedDIfferent = password.equals(repeatPassword);
+            checkedLength = password.length() >= PASSWORD_LENGTH
+                    && repeatPassword.length() >= PASSWORD_LENGTH;
+        }
+        if (!checkedDIfferent || !checkedLength) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
