f92c347d2aa2dad09e562f726222a6eb46d7f533
Sasha Vradiy <alexvradii@gmail.com>
2022-07-07 16:55:22 +0300

    My solution of tusk checked exception.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,6 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null
-                || repeatPassword == null
                 || !password.equals(repeatPassword)
                 || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
