fca08a652494d0f69cc38b2c5decd65d8314011c
Andrii Malenko <andmalenko@gmail.com>
2022-07-12 21:53:49 +0300

    I've completed this task. Please check it.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -8,7 +8,7 @@ public class UserService {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        } catch (PasswordValidationException | NullPointerException e) {
+        } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
