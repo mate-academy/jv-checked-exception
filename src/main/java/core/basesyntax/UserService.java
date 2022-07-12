2579534607f5e8c4abaa619e7e2bb9ea92fd11dd
Andrii Malenko <andmalenko@gmail.com>
2022-07-12 21:49:57 +0300

    I've completed this task. Please check it.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
+    public void registerUser(User user) {
+
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException | NullPointerException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
