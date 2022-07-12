edf0941004c97f86a2ae3fd9e40615043fdc3b7c
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-12 11:53:50 +0300

    line 6,7 in Class PasswordValidator changed to if  (password == null || repeatPassword == null || !password.equals(repeatPassword) || password.length() < 10)  {

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() < 10 || password == null
-                || repeatPassword == null) {
+        if  (password == null || repeatPassword == null || !password.equals(repeatPassword)
+                || password.length() < 10)  {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
