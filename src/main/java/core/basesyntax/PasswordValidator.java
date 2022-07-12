8f3cd8b25c9c05fc3211f4aebb352a56792378b8
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-12 11:18:01 +0300

    Class PasswordValidator line 7 - corrected formatting error

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,6 +4,7 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (!password.equals(repeatPassword) || password.length() < 10) {
-            throw new PasswordValidationException("Wrong passwords");}
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
