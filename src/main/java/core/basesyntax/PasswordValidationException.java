e2accfc662b6f6e9cf14c96aeabc00ed286f23a4
Ivan Litvynets <Litvyvanya@gmail.com>
2022-07-24 23:36:00 +0300

    Added exception & validate method

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,9 @@
 package core.basesyntax;
 
-//write your code here
+import java.io.IOException;
+
+class PasswordValidationException extends Exception {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
