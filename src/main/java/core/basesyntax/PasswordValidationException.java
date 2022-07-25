b25e1dad8bc0263cd3949fe339b24e4e2d2a4e49
oleksandr bratashchuk <olbratashchuk03@gmail.com>
2022-07-25 18:10:48 +0300

    Fix style

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 //write your code here
 public class PasswordValidationException extends Exception {
-  public PasswordValidationException(String message) {
-    super(message);
-  }
+    public PasswordValidationException(String message) {
+        super(message);
+    }
 }
