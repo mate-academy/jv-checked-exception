bfa5b6bf6070b85c5818b630e08cf4c60375cb88
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-26 20:28:43 +0200

    add .registerUser() method

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public void registerUser(User user) {
+        try {
+            new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println(("Wrong passwords"));
+        }
     }
 
     public void saveUser(User user){
