c23692538041c672c26cda9c43b5b474cf1da622
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-26 21:04:48 +0200

    Update PasswordValidator.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword) 
+        throws PasswordValidationException {
         if (password.length() < 10 && repeatPassword.length() < 10
-                && (password == null || repeatPassword == null) ) {
+                && (password == null || repeatPassword == null)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
