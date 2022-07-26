da1020f0c4a8d4efe6719bd32ee590cbc3728f6b
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-26 11:52:12 +0300

    Create PasswordValidationException for UserService

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MAX_LENGTH_OF_PASSWORD = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        try {
+            if (password.length() < MAX_LENGTH_OF_PASSWORD || !password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (NullPointerException e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
