a342213c3e81dc3ee8880e53202ac2fc2516a54b
Nazar <litvik.12@gmail.com>
2022-07-28 18:44:06 +0300

    changed logic of validation

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.io.IOException;
+
 public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
