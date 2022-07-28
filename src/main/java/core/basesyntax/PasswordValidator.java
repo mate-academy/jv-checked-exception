18f2d09a5503c1dbc8da87cf15453ce9b941544c
Veers <raadch3011@gmail.com>
2022-07-28 19:48:08 +0300

    implemented required methods

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,9 +5,9 @@ public class PasswordValidator {
             throws PasswordValidationException {
 
         if ((password == null || repeatPassword == null)
-                || password.equals(repeatPassword)
+                || !(password.equals(repeatPassword)
                 || password.length() >= 10
-                || repeatPassword.length() >= 10) {
+                || repeatPassword.length() >= 10)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
