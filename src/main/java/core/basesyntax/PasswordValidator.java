fa908f96917dad997a11881430384e91a88330f5
Veers <raadch3011@gmail.com>
2022-07-28 19:26:00 +0300

    implemented recauired methods

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword){
         try {
-            if ((password == null || repeatPassword == null)
-            || password.equals(repeatPassword) || password.length() >= 10 || repeatPassword.length() >= 10) {
-                throw new PasswordValidationException("Wrong passwords");
-            }
-        } catch(PasswordValidationException e) {
+                if ((password == null || repeatPassword == null)
+                    || password.equals(repeatPassword)
+                    || password.length() >= 10
+                    || repeatPassword.length() >= 10) {
+                    throw new PasswordValidationException("Wrong passwords");
+                }
+        } catch (PasswordValidationException e) {
             System.out.println("Wrong passwords");
         }
     }
