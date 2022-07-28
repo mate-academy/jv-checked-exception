dffc88a4f13fff03b34defe10747754e820ca0db
Veers <raadch3011@gmail.com>
2022-07-28 19:06:58 +0300

    implemented recauired methods

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword){
         try {
-            if (password.equals(repeatPassword) && password.length() <= 10 && repeatPassword.length() <= 10) {
-
-            }
-            else {
+            if ((password == null || repeatPassword == null)
+            || password.equals(repeatPassword) || password.length() >= 10 || repeatPassword.length() >= 10) {
                 throw new PasswordValidationException("Wrong passwords");
             }
         } catch(PasswordValidationException e) {
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
+    public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
         this.saveUser(user);
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
