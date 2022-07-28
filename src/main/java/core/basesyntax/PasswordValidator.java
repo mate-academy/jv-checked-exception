c9d72273d34618512f857c680b70eec2e6734d62
Veers <raadch3011@gmail.com>
2022-07-28 19:26:06 +0300

    implemented recauired methods

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword){
+    public void validate(String password, String repeatPassword) {
         try {
                 if ((password == null || repeatPassword == null)
                     || password.equals(repeatPassword)
