50ae258ace5fe22479d62245e3d170f9a5e6529f
Vasyl Pankiv <wasilius2@gmail.com>
2022-07-29 18:19:15 +0300

    Implement user password validation before saving in database

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASSWORD_LENGTH = 10;
+    UserService userService = new UserService();
+    public void validate(String password, String repeatPassword) throws PasswordValidationException{
+        if (password.length() < MIN_PASSWORD_LENGTH && !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
