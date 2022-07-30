a2059653ac939b1eee08261a41cb7623b23b377c
Aptypio13 <aptypio668@gmail.com>
2022-07-30 17:58:35 +0300

    fix checkstyle and validate method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null
-                || password.length() >= 10 || repeatPassword.length() >= 10
+    public void validate(
+            String password, String repeatPassword) throws PasswordValidationException {
+        if (
+                password == null
+                || password.length() < 10 || repeatPassword.length() < 10
                 || !password.equals(repeatPassword)) {
-             throw new PasswordValidationException("Wrong passwords");
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
