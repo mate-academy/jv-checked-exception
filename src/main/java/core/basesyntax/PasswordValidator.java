23b5b5353a294fc3862cf9424eedfd6a41da23a8
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-01 15:06:53 +0300

    PASSWORD_LENGTH made as constant

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+    private static final int PASSWORD_LENGTH = 10;
+
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null
                 || !password.equals(repeatPassword)
-                || password.length() < 10) {
+                || password.length() < PASSWORD_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
