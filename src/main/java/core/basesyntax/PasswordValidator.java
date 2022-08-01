e80f0d480e97d5dbe625fd250a599585525e276f
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-01 16:09:02 +0300

    check style

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password != null
                 && repeatPassword != null
                 && password.length() >= MIN_PASSWORD_LENGTH
