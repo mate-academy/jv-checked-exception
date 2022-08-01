1860d636f791a5a62a2013c47047dd989aa7487d
Dasha Horun <dhorunka@gmail.com>
2022-08-02 00:47:29 +0200

    all methods are implemented

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,9 @@
 package core.basesyntax;
 
-//write your code here
+public class PasswordValidationException extends Exception{
+    public PasswordValidationException (String message) {
+        super(message);
+
+    }
+
+}
