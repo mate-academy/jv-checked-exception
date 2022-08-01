ec42094979af91651b9387925e43e234d88b53f3
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-01 16:13:02 +0300

    change method validate in PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,11 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password != null
-                && repeatPassword != null
-                && password.length() >= MIN_PASSWORD_LENGTH
-                && repeatPassword.length() >= MIN_PASSWORD_LENGTH
-                && password.equals(repeatPassword)) {
-        } else {
+        if (password == null
+                || repeatPassword == null
+                || password.length() < MIN_PASSWORD_LENGTH
+                || repeatPassword.length() < MIN_PASSWORD_LENGTH
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
