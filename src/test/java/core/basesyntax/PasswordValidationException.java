e0aa5ec7e77856c9b505f427b001971afd30d87c
UmyaFamilia <umyafamilya1974@gmail.com>
2022-08-02 20:23:34 +0300

    this progtam first try

--- src/test/java/core/basesyntax/PasswordValidationException.java
+++ src/test/java/core/basesyntax/PasswordValidationException.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.IOException;
 
-//write your code here
 public class PasswordValidationException extends IOException {
     public PasswordValidationException(String message) {
         super(message);
