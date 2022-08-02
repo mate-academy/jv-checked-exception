6f05a2c5be47240cec51cdfe5237384a88d93c65
Julia Kononenko <munguryan13@gmail.com>
2022-08-02 13:55:54 +0300

    implemented validator

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    private final PasswordValidator passwordValidator;
+
+    public UserService(PasswordValidator passwordValidator) {
+        this.passwordValidator = passwordValidator;
     }
 
-    public void saveUser(User user){
+    public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
