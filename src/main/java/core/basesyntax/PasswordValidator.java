03c7b32e9a6589e7db037f1d84e8fa644323ccf3
IraHurko <iragurko19@gmail.com>
2022-08-02 13:09:07 +0300

    created new Exception for registerUser method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (repeatPassword == null || password == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!(repeatPassword.equals(password) && password.length() >= 10)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
