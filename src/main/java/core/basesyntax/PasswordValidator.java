7a8c1015e3f7ef9a01f3f0e722540b60a86ab7c2
Liubomyr Motsiak <motsiakliubomyr@gmail.com>
2022-08-07 00:21:57 +0300

    1. completed method validate(): compared password with repeatPassword and checked if their length is more or equal to 10. If not, threw exception PasswordValidationException 2. created checked exception PasswordValidationException 3. completed method registerUser(): called the method validate() and handled the exception. If everything is ok, save user to the database, if not, print the message

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public final void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == repeatPassword
+                && password != null
+                && repeatPassword != null
+                && password.length() >= 10
+                && repeatPassword.length() >= 10) {
+            return;
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
