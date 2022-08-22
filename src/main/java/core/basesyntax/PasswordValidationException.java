251060e57c1d97f655f855b3eb2bd3cc31a56032
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-22 14:55:02 +0300

    checkstyle changes

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
-    public PasswordValidationException (String massage) {
+    public PasswordValidationException(String massage) {
         super(massage);
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class UserService {
     private final PasswordValidator passwordValidator = new PasswordValidator();
 
-
-        public void registerUser(User user) {
-            try {
-                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+    public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
                 saveUser(user);
             } catch (PasswordValidationException e) {
                 System.out.println("Your passwords are incorrect. Try again.");
