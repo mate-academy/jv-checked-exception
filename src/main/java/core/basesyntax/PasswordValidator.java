061fa8e00074791ee12e907e075b87586f472984
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-22 15:00:50 +0300

    checkstyle changes

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null
-        || repeatPassword == null
-        || password.length() < 10
-        || repeatPassword.length() < 10
-        || repeatPassword.equals(password) == false) {
-           throw new PasswordValidationException("Wrong passwords");
+            || repeatPassword == null
+            || password.length() < 10
+            || repeatPassword.length() < 10
+            || repeatPassword.equals(password) == false) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -8,8 +8,8 @@ public class UserService {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
-                System.out.println("Your passwords are incorrect. Try again.");
-            }
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
