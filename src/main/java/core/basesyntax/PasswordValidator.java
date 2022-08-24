a5446002401277dde3f7153a21dd412d9f6e8afc
Valeriy M <valeriym250@gmail.com>
2022-08-24 14:13:28 +0300

    Implemented validate method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
+
+        try {
+            if (password.equals("")) {
+                throw new PasswordValidationException("Empty input data");
+            } else if (password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (!password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+
+        } catch (NullPointerException e) {
+            throw new PasswordValidationException("Password can`t be null");
+        }
     }
 }
