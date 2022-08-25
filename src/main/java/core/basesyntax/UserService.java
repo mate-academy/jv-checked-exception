87aff93b4ec9517a11626517dc22e47929e13fd3
Andrii Petruk <andrzej.petruk@bk.ru>
2022-08-25 20:30:42 +0200

    the task was completed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
-    public static final PasswordValidator passwordValidator = new PasswordValidator();
+    public final PasswordValidator passwordValidator = new PasswordValidator();
 
-    public void registerUser(User user) {
+    public void registerUser(User user) throws PasswordValidationException {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
-            System.out.println("Your passwords are incorrect. Try again.");
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         }
     }
 
