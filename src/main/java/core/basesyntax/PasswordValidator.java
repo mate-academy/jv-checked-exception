25039a15491d03422c98d7309adb177498808c81
Oleg Pruh <olegpruh1@gmail.com>
2022-08-30 15:29:41 +0100

    A swhitepace was added and the vealidate ode was replacedd

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,15 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null
-                || password.length() != repeatPassword.length()
-                || password.length() < 10 || !password.equals(repeatPassword)) {
+        try {
+            if (password == null || repeatPassword == null) {
+                throw new PasswordValidationException("Null input data");
+            } else if (!password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (PasswordValidationException e) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
