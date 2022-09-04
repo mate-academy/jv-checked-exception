3d24af8aa89990bf4c3f7f06601e278dd4b81287
sasha <radovietsoleksandr@gmail.com>
2022-09-04 14:16:27 +0300

    ex1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)throws PasswordValidationException {
+        boolean check =  password.equals(repeatPassword);
+        if (password.length() <= 10 || repeatPassword.length() <= 10){
+           throw  new PasswordValidationException("Your length is not incorrect. Try again.");
+        } else if (!check){
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,23 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public void registerUser(User user) {
+
+        if (user != null) {
+            PasswordValidator passwordValidator = new PasswordValidator();
+            try {
+                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+                saveUser(user);
+            } catch (PasswordValidationException e) {
+                System.out.println("Pleace! Try Again " + e);
+            }
+        }
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
+
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
+
+
 }
