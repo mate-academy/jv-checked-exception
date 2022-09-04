d0ee762cd35aab8d7d27c56cd9fea89b94a3f3a7
sasha <radovietsoleksandr@gmail.com>
2022-09-04 14:29:42 +0300

    ex2

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
-public class PasswordValidationException extends Exception{
+
+public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword)throws PasswordValidationException {
-        boolean check =  password.equals(repeatPassword);
-        if (password.length() <= 10 || repeatPassword.length() <= 10){
-           throw  new PasswordValidationException("Your length is not incorrect. Try again.");
-        } else if (!check){
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        boolean check = password.equals(repeatPassword);
+        if (password.length() <= 10 || repeatPassword.length() <= 10) {
+            throw new PasswordValidationException("Your length is not incorrect. Try again.");
+        } else if (!check) {
             throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         }
     }
