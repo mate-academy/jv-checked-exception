d99a17df546b0e73d27cfea655c76e4b01388bf3
sasha <radovietsoleksandr@gmail.com>
2022-09-05 15:35:10 +0300

    ex21

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,11 +2,8 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("login@email","Test_123d45","Test_12345");
+        User user = new User("login@email", "Test_123d45", "Test_12345");
         UserService userService = new UserService();
-
         userService.registerUser(user);
-
-
     }
 }
