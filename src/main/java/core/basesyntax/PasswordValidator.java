bb707891b5914e1b494d3c1aa4012743249b5d60
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-06 11:11:14 +0300

    Version 3

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (! password.equals(repeatPassword) || password.length() < 10) {
+        if (password == null || ! password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
