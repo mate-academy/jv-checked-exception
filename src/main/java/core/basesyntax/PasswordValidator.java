54302cd476d978113024af12b2da96a0638ad332
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-09-19 16:14:03 +0300

    before maven

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if(!(password.equals(repeatPassword) && (password.length() > 10))) {
+        if(password == null || password.length() < 10 ||
+                !(password.equals(repeatPassword))) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
