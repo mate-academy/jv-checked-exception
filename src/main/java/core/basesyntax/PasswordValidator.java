e34f9a8ae35fb68e72db6036e6d35889d9b3fd69
Oleksii Lytvynenko <oleksiila@gmail.com>
2022-09-19 01:21:17 +0100

    hw-checked-exception solution

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        try {
+            if (password.equals(repeatPassword) == false
+                    || repeatPassword.length() < 10
+                    || password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (NullPointerException e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
