069e150f3c11c37baa2bb4a4e67ecc8fe7dcf33b
DY-JS <delo1604@ukr.net>
2022-09-24 02:12:15 +0300

    Add solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user){
-        //write your code here
+    public void registerUser(User user) {
+        String password = user.getPassword();
+        String repeatPassword = user.getRepeatPassword();
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(password, repeatPassword);
+            saveUser(user);
+        } catch(Exception e){
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user){
