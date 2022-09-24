0459843c6b405c7ede694faf66f65010507af32c
DY-JS <delo1604@ukr.net>
2022-09-24 18:34:59 +0300

    Add solution

---
+++ src/main/java/core/basesyntax/CheckClass.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class CheckClass {
+    public static void main (String[] args) {
+       User v = new User("test@gmail.com", "qwerrtyuui", "qwerrtyuui");
+        UserService userService = new UserService();
+        userService.registerUser(v);
+    }
+}
