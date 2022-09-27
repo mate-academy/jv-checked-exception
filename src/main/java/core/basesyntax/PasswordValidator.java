77f0833b160efea5ac17485bb15753497d9df55b
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-09-27 19:26:11 +0300

    registerUser was realised

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+
+    private final int passMinLength = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (!password.equals(repeatPassword) || password.length() < passMinLength) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
