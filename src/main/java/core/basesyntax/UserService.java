9673d016d210f6fa07b3b8b6400af66bc771100b
maksim-untilov <maksimuntilov7@gmail.com>
2022-09-30 14:50:54 +0300

    Method validate() should compare the password with repeatPassword and check if their length is more or equal to 10. If not, throw exception PasswordValidationException with message "Wrong passwords" (since it's our custom logic, let's create our own, CHECKED, exception because user can put any password and our service shouldn't be broken. We should handle not-valid case and return the understandable message).
    Method registerUser() should call the method validate() and handle the exception. If everything is ok, save user to the database (use method saveUser()), if not, print the message "Your passwords are incorrect. Try again.".

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
+    private static final PasswordValidator PASSWORD_VALIDATOR = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        UserService userService = new UserService();
+
+        try {
+            PASSWORD_VALIDATOR.validate(user.getPassword(),
+                    user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
