6ec0f02e8c434b7cf7f3a51428a7b9f566909833
Viktoriia Maherko <viktoriiamaherko@gmail.com>
2022-10-01 18:59:25 +0300

    Create User Service

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < MIN_PASWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
