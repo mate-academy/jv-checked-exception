d9cc63b1cf02bf53214dfc971364b8cd443bf8aa
Andrii Matveev <matveev.tesla@gmail.com>
2022-10-01 21:25:32 +0300

    hm1 - exception has been done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,14 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        try {
+            PasswordValidator validator = new PasswordValidator();
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (Exception e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+            return;
+        }
+        this.saveUser(user);
     }
 
     public void saveUser(User user) {
