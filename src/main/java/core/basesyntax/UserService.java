21600e11fdf082d8ff32a6578af343f176511148
Veronika Tretiak <nikatretiak9@gmail.com>
2022-10-03 11:14:03 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,10 +4,10 @@ public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
-           passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-           saveUser(user);
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
-            System.out.println ("Your passwords are incorrect. Try again.");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
 
