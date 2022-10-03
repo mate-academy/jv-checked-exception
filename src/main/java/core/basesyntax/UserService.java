9326107565080bc19a10174b13c9811f19db6f12
Iryna Chernikova <i.chernikova14@ukr.net>
2022-10-03 23:14:41 +0300

    implemented task

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
+    private static final String NEW_PASSWORD = "Password#123";
+    private static final String REPEAT_NEW_PASSWORD = "Password#123";
+    private static final String EMAIL = "email@email";
+    private PasswordValidator passwordValidator = new PasswordValidator();
+    private User user = new User(EMAIL, NEW_PASSWORD, REPEAT_NEW_PASSWORD);
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
