1ff07f06743101de85ed0f9252705e6dd539e011
Luka Lozovyk <lukalozovyk@gmail.com>
2022-10-04 13:08:12 +0300

    Created checked exception. Methods validate() and registerUser() implemented.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Wrong passwords");
+            System.exit(0);
+        } finally {
+            saveUser(user);
+        }
     }
 
     public void saveUser(User user) {
