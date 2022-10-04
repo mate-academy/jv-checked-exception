5ba4690774ffe02c87fb05f1b54812368f35333a
Roman Novosad <roman.novosad@yahoo.com>
2022-10-04 15:12:36 +0200

    added solution to task Password Validation / Exception / Java Core

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    static final int MIN_PASSWORD_SIZE = 10;
+    private boolean validPassword;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password != null && repeatPassword != null & password.equals(repeatPassword)
+                & password.length() >= MIN_PASSWORD_SIZE) {
+            validPassword = true;
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
