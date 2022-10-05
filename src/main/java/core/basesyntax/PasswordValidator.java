e9cd4cbf73b8176e121b81270448edda62ac12ee
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-10-05 13:11:15 +0300

    Fixed version 4

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,7 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || !(password.equals(repeatPassword)) || password.length() > 10) {
+        if (password == null || !(password.equals(repeatPassword)) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
