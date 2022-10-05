eab694f349b7611ad5e6ce574feba08cbdfa47ea
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-10-05 09:55:32 +0300

    Fixed mvn second

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,13 +1,10 @@
 package core.basesyntax;
 
 public class UserService {
-    private  String password;
-    private String repeatPassword;
-
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
-            passwordValidator.validate(password, repeatPassword);
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
         } catch (PasswordValidationException e) {
             throw new RuntimeException("Your passwords are incorrect. Try again");
         }
