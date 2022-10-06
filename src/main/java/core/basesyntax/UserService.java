b33089af13fcca68259ed1c9fdcf226fb0c3f20f
Sachamunn <denhackgaming@gmail.com>
2022-10-06 16:56:45 +0100

    Main task completed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
+
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
