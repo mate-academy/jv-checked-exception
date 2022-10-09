3d5762b7386d168eb41b83c1e2788312f242246e
fantom <mironovanton5511@gmail.com>
2022-10-09 12:22:16 +0300

    my try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MAX_PASS_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null
+                || (password.length() < MAX_PASS_LENGTH
+                && repeatPassword.length() < MAX_PASS_LENGTH)
+                || password.isEmpty() || repeatPassword.isEmpty()) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        // this if-block I need to check, is passwords equals or not
+        if (password.equals(repeatPassword)) {
+            System.out.print("");
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
