808871eb78ee582ed5f74b3aa11eb7098ef6cd1f
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-12 23:45:02 +0200

    jv-checked_exception task done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!password.equals(repeatPassword)
+        if ((password == null && repeatPassword == null) || !password.equals(repeatPassword)
                 || (password.length() < 10
-                && repeatPassword.length() < 10)
-                || (password == null && repeatPassword == null)) {
+                && repeatPassword.length() < 10)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
