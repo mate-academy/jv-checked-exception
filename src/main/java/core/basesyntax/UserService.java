54b740032f2099ef87ada8d6b28b6c1f988f8e5e
AndriySichniy <sichniy.andriy@gmail.com>
2022-10-21 15:21:12 +0300

    2nd commit - implement PasswordValidator and UserService

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,13 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
