a579f98a0c4e2bc4c23aaeb0816ae188fba16f71
AndriySichniy <sichniy.andriy@gmail.com>
2022-10-21 15:59:58 +0300

    3nd commit - mvn recom

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
-        if (!repeatPassword.equals(password) || password.length() < 10) {
+        if (password == null || repeatPassword == null || !repeatPassword.equals(password) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
