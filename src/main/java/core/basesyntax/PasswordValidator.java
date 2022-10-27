ef2a257ade0f26c811c97bff55375e109fc363b6
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-27 15:20:38 +0200

    Indentation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,17 +3,24 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws Exception {
         try {
-        if (password.length() >= 10 && repeatPassword.length() >= 10
-                && password.length() == repeatPassword.length()) {
-            System.out.println("Right password");
-        }
+
+            if (password.length() >= 10 && repeatPassword.length() >= 10
+
+                    && password.length() == repeatPassword.length()) {
+
+                System.out.println("Right password");
+
+            }
 
         } catch (Exception e) {
 
             if (password.length() < 10 && repeatPassword.length() < 10
+
                     && password.length() != repeatPassword.length()) {
-            throw new Exception("Wrong password");
-           }
+
+                throw new Exception("Wrong password");
+
+            }
         }
     }
 
