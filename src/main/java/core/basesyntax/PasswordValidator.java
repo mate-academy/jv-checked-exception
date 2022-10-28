cf030abe5c1fbdf07f4bb8fdc4b8caa15f893682
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-28 12:38:11 +0200

    added PasswordValidation Exception deleted catch

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,23 +1,17 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws Exception {
-        try {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
 
-            if (password == null || repeatPassword == null || password.length() < 10
+        if (password == null || repeatPassword == null || password.length() < 10
 
-                    || repeatPassword.length() < 10
+                || repeatPassword.length() < 10
 
-                    || password.length() != repeatPassword.length()) {
+                || password.length() != repeatPassword.length()) {
 
-                System.out.println("Wrong Password");
-
-            }
-
-        } catch (RuntimeException e) {
-
-            throw new PasswordValidationException("Wrong password");
+            System.out.println("Wrong Password");
 
         }
+
     }
 }
