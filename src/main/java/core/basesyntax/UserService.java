8c816c4f8a62480047424149736afad9034413a2
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-28 12:26:46 +0200

    added PasswordValidation Exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,13 +2,15 @@ package core.basesyntax;
 
 public class UserService {
 
-    public void registerUser(User user) throws Exception {
+    public void registerUser(User user) throws PasswordValidationException {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect.Try again later");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
     }
