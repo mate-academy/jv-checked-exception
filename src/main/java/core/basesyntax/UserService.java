b6b33727ee6b8f7cac7a1afa3124cd339bc593b6
Oksana Domanchuk <dvoynikova@gmail.com>
2022-11-01 10:18:12 +0200

    handled nullPointerException in PasswordValidator class

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-        if (user.getPassword() != null || user.getRepeatPassword() != null) {
-            try {
-                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-                saveUser(user);
-            } catch (PasswordValidationException e) {
-                System.out.println("Your passwords are incorrect. Try again.");
-            }
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
 
