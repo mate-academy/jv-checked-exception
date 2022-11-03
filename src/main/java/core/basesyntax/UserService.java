ff1c69166abee5f6aae93b0011668bca6770a20e
Diana <dlomei283@gmail.com>
2022-11-03 18:10:50 +0200

    done.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,14 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        try{
+            validate();
+        } catch (Exception e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        } saveUser(user);
+    }
+
+    private void validate() {
     }
 
     public void saveUser(User user) {
