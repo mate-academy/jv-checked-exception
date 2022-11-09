37aa99c55a4328c8429bfbd21a46a2c8357c644f
Petro Korets <petrkorets@gmail.com>
2022-11-09 10:12:13 +0200

    Implemented validate() and registerUser() methods. Extended PasswordValidationException from Exception and dropped it for non-valid cases in validate() method.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Password is null");
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if(password.length() < 10) {
+            throw new PasswordValidationException("Password's length is less than 10");
+        }
+        if (password.equals("")) {
+            throw new PasswordValidationException("Empty password");
+        }
     }
 }
