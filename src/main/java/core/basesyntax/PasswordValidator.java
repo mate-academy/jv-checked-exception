cd5779ee080083762e78774e1bc864d4a7fe4004
Svitlana Tertyshna <svitla.ter@gmail.com>
2022-11-13 16:01:34 +0200

    Implemented method, created exception; all tests & mvn check done.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int MIN_NUMBER_OF_CHARECTERS = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null || password.length() < MIN_NUMBER_OF_CHARECTERS
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
