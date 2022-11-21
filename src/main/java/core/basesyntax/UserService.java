6f6ecfbd1f2d563bc8250a7f6d70e1ddb67d238c
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-11-21 13:57:20 +0200

    created exception class with constructor that can show message to console, implemented methods registerUser(), & validate().

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
+    public void registerUser(User user) throws PasswordValidationException {
         //write your code here
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (Exception e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+
+
     }
 
     public void saveUser(User user) {
