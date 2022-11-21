ec0947bfde8d855654a2399ce1592d1eed7fec1a
Artem Otsabera <eskvair86@gmail.com>
2022-11-21 22:19:27 +0200

    add method in PasswordValidationException class checked validate method checked registerUser method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    static final int MAX_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < MAX_LENGTH) {
+
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
