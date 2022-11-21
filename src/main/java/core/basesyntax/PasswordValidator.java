aa6313c5632873f69ab47bfcf305fc242f347970
Dima Byrka <fobossen@gmail.com>
2022-11-21 16:47:17 +0200

    Build is working

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        //write your code her
+        if ((password != null || repeatPassword != null)
+                && (password.equals(repeatPassword)) && (password.length() >= 10)) {
+            String correct = "correct";
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
