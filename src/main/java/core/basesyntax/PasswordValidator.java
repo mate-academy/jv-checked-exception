2088b741e3e2c85fdeb901ab83345c7632eab678
user <graban2253@gmail.com>
2022-11-26 01:08:23 +0200

    Finished

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        if (!password.equals(repeatPassword) || password.length() < 10
-                || repeatPassword.length() < 10) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Empty data");
+        }
+
+        if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
