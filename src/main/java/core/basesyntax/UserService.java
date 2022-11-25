2c631694f230a4c47f1805d5e41f341ece05c8f3
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-11-25 21:40:06 +0300

    solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,7 +7,7 @@ public class UserService {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
-            throw new PasswordValidationException("Wrong passwords");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
         saveUser(user);
     }
