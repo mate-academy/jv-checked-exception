1fc2b4f6be7337aa13e79dc45edd5fec87169f1f
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-11-25 21:32:44 +0300

    solution

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -2,6 +2,6 @@ package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
-        super(message);
+            super(message);
     }
 }
