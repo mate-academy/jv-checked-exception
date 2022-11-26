a5a857800d3c9115520645791f5a5bb6ce12bbc4
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-11-26 19:38:48 +0200

    solution of jv checked exception5

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,18 +1,19 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    private boolean isEqual;
+    private boolean isCorrect;
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.equals(repeatPassword)) {
-            isEqual = true;
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
         }
-        if (!isEqual
-                || password.length() < 10
-                || repeatPassword.length() < 10
-                || password == null
-                || repeatPassword == null) {
+        if (password.equals(repeatPassword)
+                && password.length() >= 10
+                && repeatPassword.length() >= 10) {
+            isCorrect = true;
+        }
+        if (isCorrect == false) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
