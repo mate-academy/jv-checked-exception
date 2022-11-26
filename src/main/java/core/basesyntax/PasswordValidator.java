162443dcb1671481b460dff1a94e826b0bb5bbf6
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-11-26 20:55:15 +0200

    solution of jv checked exception10try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 public class PasswordValidator {
     private boolean isEqual;
     private boolean isLongEnough;
+    private static final PasswordValidationException p = new PasswordValidationException("Wrong passwords");
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Wrong passwords");
+            throw p;
         }
         if (password.equals(repeatPassword)) {
             isEqual = true;
@@ -15,11 +16,8 @@ public class PasswordValidator {
         if (password.length() >= 10 && repeatPassword.length() >= 10) {
             isLongEnough = true;
         }
-        if (isEqual != true) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-        if (isLongEnough != true) {
-            throw new PasswordValidationException("Wrong passwords");
+        if (isEqual != true  || isLongEnough != true) {
+            throw p;
         }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator validator = new PasswordValidator();
+    private final PasswordValidator validator = new PasswordValidator();
 
-    public void registerUser(User user) {
+    public void registerUser(User user) throws PasswordValidationException {
         try {
             validator.validate(user.getPassword(), user.getRepeatPassword());
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
+            throw e;
         }
         saveUser(user);
     }
