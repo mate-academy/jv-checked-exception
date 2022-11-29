445bda14eb9a7ff8b205eaca29e12aac42f2b68d
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-29 16:09:37 +0200

    Task jv-checked-exception1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if(password == null && repeatPassword == null){
+        if (password == null && repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if(password != repeatPassword || password.length() < 10 || repeatPassword.length() < 10){
+        if (password != repeatPassword || password.length() < 10 || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,12 +4,12 @@ public class UserService {
 
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-         try{
-             passwordValidator.validate("Password#123", "Password#123");
-             saveUser(new User("email@email","Password#123", "Password#123"));
-         } catch (PasswordValidationException e) {
-             System.out.println("Your passwords are incorrect. Try again.");
-         }
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(new User(user.getEmail(), user.getPassword(), user.getRepeatPassword()));
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
