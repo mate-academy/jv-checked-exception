e3459fde5017d3e8203aab15a7c5b08ef5377324
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-29 13:24:55 +0200

    Task jv-checked-exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+         try{
+             passwordValidator.validate("Password#123", "Password#123");
+             saveUser(new User("email@email","Password#123", "Password#123"));
+         } catch (PasswordValidationException e) {
+             System.out.println("Your passwords are incorrect. Try again.");
+         }
     }
 
     public void saveUser(User user) {
