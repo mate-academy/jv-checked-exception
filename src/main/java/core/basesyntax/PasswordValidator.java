0b3f6a0500e5f75b90939915f27820770c0e4df9
Алексей <m.leshka955@gmail.com>
2022-12-01 14:55:07 +0200

    fix checkstyle

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null && repeatPassword == null || !password.equals(repeatPassword) || password.length() < 10 && repeatPassword.length() < 10 && password.equals(repeatPassword)) {
+        if (password == null && repeatPassword == null
+                || !password.equals(repeatPassword)
+                || password.length() < 10
+                && repeatPassword.length() < 10
+                && password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
