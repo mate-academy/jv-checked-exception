3b31e46f9e0c1d5f214b0cdb9664ae7e0bee24ce
Oleksandr Klymenko <klim777angel@gmail.com>
2022-12-08 19:22:08 +0200

    checkstyle changes

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,8 @@
 package core.basesyntax;
 
-
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException{
-        //write your code here
+
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
         if (password == null || repeatPassword == null
                 || password.length() < 10 || repeatPassword.length() < 10
                 || password.equals(repeatPassword) == false) {
