bdbc6da308a4f913442f80156e25e0e0f572caed
carbo <carbon7557@gmail.com>
2022-12-12 12:29:15 +0000

    Task is completed. All tests have been passed.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,7 @@ public class PasswordValidator {
             throws PasswordValidationException {
         if (password == null || password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
-    }
 
