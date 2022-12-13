205325f618f6e95d67d2bb77c0a751340fb32b61
Artem Voitiuk <artemon2504@gmail.com>
2022-12-13 14:54:53 +0200

    Password check implement

--- src/main/java/core/basesyntax/User.java
+++ src/main/java/core/basesyntax/User.java
@@ -37,10 +37,10 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{"
-            + "email='" + email + '\''
-            + ", password='" + password + '\''
-            + ", repeatPassword='" + repeatPassword + '\''
-            + '}';
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                '}';
     }
 }
