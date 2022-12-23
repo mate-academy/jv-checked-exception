191806817b901ce6e983b6178a68e4b35d8cd0cb
Lottery <33884443dfyz@gmail.com>
2022-12-23 15:03:00 +0200

    remove unnecessary repeat password null check

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import java.util.Locale;
-
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null |!password.equals(repeatPassword) | password.length() < 9) {
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
