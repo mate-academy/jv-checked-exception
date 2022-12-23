c1613be63ae74f5059f34bbb4941066f6af3a04a
Lottery <33884443dfyz@gmail.com>
2022-12-23 13:23:36 +0200

    READY

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Locale;
+
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null |!password.equals(repeatPassword) | password.length() < 9) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
