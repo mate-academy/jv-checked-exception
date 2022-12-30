0785d481eab11145de84f3096adb4394729be5df
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2022-12-30 11:03:52 +0200

    hw checked exception long line  fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_PASSWORD_LETTERS = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null | repeatPassword == null) {
             throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         } else if (!password.equals(repeatPassword)) {
