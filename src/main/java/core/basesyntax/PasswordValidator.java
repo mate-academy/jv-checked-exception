00b8f3709b7d62ac35a5232b25eec5f42015c7a0
Denys Dominskyi <dominskyj@gmail.com>
2023-01-03 23:41:24 +0200

    Solution2

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null) {
             throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         } else if (!(password.equals(repeatPassword))) {
