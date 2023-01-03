278afe3491409d3368f8b06b63a8d797754f0eb4
Igor Parovenko <igorparovenko@yahoo.com>
2023-01-03 16:21:35 +0100

    methods implemented

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final String EXCEPTION_MESSAGE = "Wrong passwords";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException(EXCEPTION_MESSAGE);
+        }
     }
 }
