a34c14a31d144d5877aec48809488af51f6dbe48
Anna Melnychuk <annamelnichuk2512@gmail.com>
2023-01-07 17:46:15 +0200

    Solution PasswordValidation3

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,9 +4,9 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-            if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
-                throw new PasswordValidationException("Wrong passwords");
-            }
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
 
     }
 }
