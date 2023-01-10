b12bca395f50c84d9faef99b976f052af899f0df
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-10 23:47:18 +0200

    realised metod validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (Objects.equals(password, null)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
     }
 }
