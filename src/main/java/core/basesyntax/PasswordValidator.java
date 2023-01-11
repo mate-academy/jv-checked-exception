c3c11c84ff59f3f57ea0d388c78c6af98a23db72
Artur Muliukov <arthur.muliukov@gmail.com>
2023-01-11 22:18:02 +0500

    Solved 1.5

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.equals(repeatPassword) || password.length() <= 10
-                || repeatPassword.length() <= 10) {
-            System.out.println("Congrats");
-        } else {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) throws PasswordValidationException {
+    public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-            UserService userService = new UserService();
-            userService.saveUser(user);
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
