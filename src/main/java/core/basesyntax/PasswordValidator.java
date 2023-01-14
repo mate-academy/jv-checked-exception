1ccb72f9d6ad8b770794a01fa68f6e997195e7fd
Maxim Skory <devisrush@gmail.com>
2023-01-14 03:57:20 +0200

    registerUser() method logic was implemented, validate() method was fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || password.isEmpty() || !password.equals(repeatPassword) || password.length() >= 10) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again");
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
