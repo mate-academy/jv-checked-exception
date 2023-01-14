53b5f219fccb26c391a2ec856e9c99189d84b79b
Maxim Skory <devisrush@gmail.com>
2023-01-14 03:24:20 +0200

    validate() method logic was implemented

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || password.isEmpty() || !password.equals(repeatPassword) || password.length() >= 10) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again");
+        }
     }
 }
