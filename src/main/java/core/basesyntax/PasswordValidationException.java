03390a215445dcc311082dad554d33bb3cb23ce9
IhorPetrytskyi333 <mygit333@gmail.com>
2023-01-19 21:33:53 -0800

    solution_1

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,8 @@
 package core.basesyntax;
+class PasswordValidationException extends Exception{
 
-//write your code here
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
+
