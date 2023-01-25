b9c9fdee5ba5daeee0ce253cce4e8579c9c3f16c
Artem Ahapov <artemagapov777@gmail.com>
2023-01-25 21:18:34 +0200

    hw done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    static final PasswordValidator pw = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            pw.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
