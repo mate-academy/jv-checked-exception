7c0120f9fb6f77b3be2f77085d1e74cb465e6e9d
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-25 19:41:13 +0200

    Created class PasswordValidationException and method printMessage()

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,14 @@
 package core.basesyntax;
 
-//write your code here
+public class PasswordValidationException extends Exception {
+    private String message;
+
+    public PasswordValidationException(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
+    }
+}
+
