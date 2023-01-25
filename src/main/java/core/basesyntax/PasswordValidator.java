cb06358af8425c96286cada97f8317a645e490ba
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-25 21:34:51 +0200

    fixed method in class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,7 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null || !password.equals(repeatPassword)
-                || password.length() >= MAX_LENGTH_PASSWORD) {
+                || password.length() <= MAX_LENGTH_PASSWORD) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
