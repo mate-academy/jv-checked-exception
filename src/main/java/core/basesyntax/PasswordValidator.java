819ddf63a2f4b87b6796150865e0447494715036
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-26 19:44:45 +0200

    fixed method validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MAX_LENGTH_PASSWORD = 10;
 
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null || !password.equals(repeatPassword)
                 || password.length() < MAX_LENGTH_PASSWORD) {
             try {
                 throw new PasswordValidationException("Wrong passwords");
             } catch (PasswordValidationException exception) {
-                System.out.println("Your passwords are incorrect. Try again.");
             }
         }
     }
