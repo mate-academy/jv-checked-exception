0e2970cbcaa6ec63e763b5e72da8867969e51bfc
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-26 17:00:12 +0200

    fixed method registerUser

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,11 +6,10 @@ public class UserService {
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-            saveUser(user);
         } catch (PasswordValidationException exception) {
-            exception.printMessage();
             System.out.println("Your passwords are incorrect. Try again.");
         }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
