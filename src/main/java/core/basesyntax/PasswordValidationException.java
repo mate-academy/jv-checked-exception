a8f8ac4cc5bcd6b63777099485afb20d3866dfd8
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-26 20:21:46 +0200

    Deleted method and field, fixed constructor

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,14 +1,9 @@
 package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
-    private String message;
 
     public PasswordValidationException(String message) {
-        this.message = message;
-    }
-
-    public void printMessage() {
-        System.out.println(message);
+        super(message);
     }
 }
 
