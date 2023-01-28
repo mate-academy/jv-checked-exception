6a9304a3cd7573067d57db04cbf4ee7110601877
yermilov.m <yermilov.m@hotmail.com>
2023-01-28 20:51:27 +0100

    implemented method validate()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,5 +3,17 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
         //write your code here
+        String message = null;
+        try {
+            if (password.length() < 10) {
+                message = "Password length is less than 10 characters";
+            }
+            else if (!(password.equals(repeatPassword))){
+                message = "Passwords doesn't match";
+            }
+            if (message != null) throw new PasswordValidationException(message);
+        } catch (PasswordValidationException p){
+            System.out.println(p.getMessage());
+        }
     }
 }
