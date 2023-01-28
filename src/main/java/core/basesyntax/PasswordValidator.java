9534cccb289961477c534264e38a2eb10a09ef0a
yermilov.m <yermilov.m@hotmail.com>
2023-01-28 20:58:47 +0100

    implemented method registerUser()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,19 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
         String message = null;
-        try {
-            if (password.length() < 10) {
-                message = "Password length is less than 10 characters";
-            }
-            else if (!(password.equals(repeatPassword))){
-                message = "Passwords doesn't match";
-            }
-            if (message != null) throw new PasswordValidationException(message);
-        } catch (PasswordValidationException p){
-            System.out.println(p.getMessage());
+        if (password.length() < 10) {
+            message = "Password length is less than 10 characters";
+        } else if (!(password.equals(repeatPassword))) {
+            message = "Passwords doesn't match";
         }
+        if (message != null) throw new PasswordValidationException(message);
     }
 }
