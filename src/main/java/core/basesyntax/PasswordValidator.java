8a4eae97402c237b8689abe5801de7585336af13
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-01-29 15:43:20 +0200

    Added RegisterUser methods and validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,5 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
         //write your code here
+        if(password.length() < 10 || password != repeatPassword) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,17 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        String password = user.getPassword();
+        String repeatPassword = user.getRepeatPassword();
+
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        try {
+            passwordValidator.validate(password,repeatPassword );
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
