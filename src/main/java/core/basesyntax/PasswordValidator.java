3e966284c7263dc26d272ddae048e20ea7f94186
kromch <94765048+kromch@users.noreply.github.com>
2023-01-30 18:28:06 +0200

    Update PasswordValidator.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) 
-	        throws PasswordValidationException {
+                 throws PasswordValidationException {
         if ((password.equals(repeatPassword) 
-	        && (password.length() > 0 && password.length() < 10)) == false) {
+	    && (password.length() > 0 && password.length() < 10)) == false) {
             throw new PasswordValidationException("Wrong passwords");
-       }
+        }
     }
 }
