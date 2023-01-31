a252a141721e257ef243353bc7ab897817ac5e50
kromch <94765048+kromch@users.noreply.github.com>
2023-01-31 22:26:23 +0200

    Update PasswordValidator.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,14 +4,14 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword) 
                  throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Your passwords are incorrect."
+            throw new PasswordValidationException("Your passwords are incorrect. "
                 + "Try again.");
         }
         if (password.equals(repeatPassword) == false) {
             throw new PasswordValidationException("Wrong passwords");
         }
         if (password.length() < 10) {
-            throw new PasswordValidationException("Your passwords are incorrect."
+            throw new PasswordValidationException("Your passwords are incorrect. "
                 + "Try again.");
         }
     }
