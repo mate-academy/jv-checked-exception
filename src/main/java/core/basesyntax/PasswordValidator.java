8d124b2e81cef6a0f8ef1b5733f96b460731891b
industrialeng <semonenko055@gmail.com>
2023-02-02 13:04:02 +0200

    task were solwed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MINIMAL_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null
+                || !repeatPassword.equals(password) || repeatPassword.length() < MINIMAL_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
