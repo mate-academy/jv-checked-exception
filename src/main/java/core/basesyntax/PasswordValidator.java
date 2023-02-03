21bdd25f681fc74f23950c1986890b3a9ba587b1
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2023-02-03 09:56:53 +0200

    Task was built

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,9 +2,12 @@ package core.basesyntax;
 
 public class PasswordValidator {
     private static final int LENGTH_PASSWORD = 10;
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || password.length() < LENGTH_PASSWORD || !password.equals(repeatPassword) {
-            throw new PasswordValidationException("Wrong password");
+
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        if (password == null || password.length() < LENGTH_PASSWORD
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
