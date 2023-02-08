19dd811c0f868dfa212678751527d4ea56891507
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-08 19:17:57 +0200

    Check password was implements

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        //write your code here
+        if (password == null || password.length() < 0 || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
