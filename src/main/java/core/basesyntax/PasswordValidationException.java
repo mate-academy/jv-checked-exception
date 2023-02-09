8e53cd8056fd117983f6038316cddd868ed217b2
Nazar Zvarych <zvarych45@gmail.com>
2023-02-09 21:58:19 +0200

    Do task

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,9 @@
 package core.basesyntax;
 
+public class PasswordValidationException extends Exception {
+    public PasswordValidationException(String string) {
+        super(string);
+    }
+}
 //write your code here
+
