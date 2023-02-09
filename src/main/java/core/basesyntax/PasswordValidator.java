16b3a6a3f035cf01a33c05d65514306712f6109b
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-09 22:44:49 +0200

    Implement method PasswordValidator, registerUser. Case ignore tests. Need mentor check)

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (password.length() == 0 || repeatPassword.length() == 0) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if(password.length() < PASSWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        try {
+            if (password.equals(repeatPassword)) {
+                return;
+            }
+        } catch (RuntimeException e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
 public class UserService {
+    PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        User user1 = new User(user.getEmail(), user.getPassword(), user.getRepeatPassword());
+        try {
+            passwordValidator.validate(user1.getPassword(), user1.getRepeatPassword());
+        } catch (RuntimeException e) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
+        saveUser(user1);
     }
 
     public void saveUser(User user) {
