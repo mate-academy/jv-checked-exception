62520f33f2f520b9ba22b954bf2f69aa5e0226bf
vojtcekhovski <111798968+vojtcekhovski@users.noreply.github.com>
2023-02-10 08:14:32 +0000

    hw exception 2

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_LENGTH = 10; 
     
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword) {
         if (password == null || repeatPassword == null 
                 || repeatPassword.length() < MIN_LENGTH || !repeatPassword.equals(password)) {
-                    throw new PasswordValidationException("Wrong password");
+            throw new PasswordValidationException("Wrong password");
         }
     }
 }
