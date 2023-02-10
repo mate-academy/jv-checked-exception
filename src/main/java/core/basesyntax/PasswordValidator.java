19c2de7325da0ad8de63679f9d0b9e038e775d51
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-10 15:12:34 +0200

    Change exception throw. Change validate method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,20 +4,12 @@ public class PasswordValidator {
     public static final int PASSWORD_LENGTH = 10;
 
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
+        if (password == null || password.length() < PASSWORD_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password.length() == 0 || repeatPassword.length() == 0) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-        if(password.length() < PASSWORD_LENGTH) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-        try {
-            if (password.equals(repeatPassword)) {
-                return;
-            }
-        } catch (RuntimeException e) {
+        if (password.equals(repeatPassword)) {
+            return;
+        } else {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,10 +7,10 @@ public class UserService {
         User user1 = new User(user.getEmail(), user.getPassword(), user.getRepeatPassword());
         try {
             passwordValidator.validate(user1.getPassword(), user1.getRepeatPassword());
-        } catch (RuntimeException e) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            saveUser(user1);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
         }
-        saveUser(user1);
     }
 
     public void saveUser(User user) {
