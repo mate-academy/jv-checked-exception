ac18de5887a6d0091e69a7329a837d554b3d7fd7
KosMarch <kosmarchenko98@gmail.com>
2023-02-11 17:23:13 +0200

    fixed common mistakes

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,15 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+    private static final int MAX_SIZE = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-
-        if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
-        if (password.equals(repeatPassword) && password.length() >= 10) {
-        }
-        else {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword) || password.length() < MAX_SIZE) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,17 +1,12 @@
 package core.basesyntax;
 
 public class UserService {
-
-
     public void registerUser(User user) {
-
         PasswordValidator passwordValidator = new PasswordValidator();
-
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        }
-        catch (PasswordValidationException e) {
+        } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
