d5e192afba2e57bc0fe9acf190199a0628ef17ad
Maksym <maxim.yashyn@gmail.com>
2023-02-14 12:48:53 +0200

    corrected check in validate() method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        try {
-            if (password.length() < 10 && !password.equals(repeatPassword)) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
+    private static final int MAX_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if ((password == null)
+                || (password.length() < MAX_LENGTH
+                && !password.equals(repeatPassword))) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
+
