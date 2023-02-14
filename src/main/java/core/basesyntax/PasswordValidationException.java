7215b4c531642ad5a89d740322bd530da527fdd1
yuriy49 <yuriy121991@gmail.com>
2023-02-14 23:43:22 +0200

    implement user password check

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,21 @@
 package core.basesyntax;
 
-//write your code here
+import java.io.IOException;
+
+class PasswordValidationException extends Exception {
+    public PasswordValidationException() {
+    }
+
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+
+    public PasswordValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PasswordValidationException(Throwable cause) {
+        super(cause);
+    }
+}
+
