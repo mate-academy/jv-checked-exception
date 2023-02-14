a2369c6883ded6abb1d52b6c3f7d63db643627cd
Maksym <maxim.yashyn@gmail.com>
2023-02-14 11:14:56 +0200

    corrected PasswordValidationException exception class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         try {
-            password.equals(repeatPassword);
+            if (password.length() < 10 && !password.equals(repeatPassword)) {
+                throw new Exception();
+            }
         } catch (Exception e) {
             throw new PasswordValidationException("Wrong passwords");
         }
