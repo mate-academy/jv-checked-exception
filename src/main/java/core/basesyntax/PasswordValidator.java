11dde079779420e3ab0adf24f157c9e1a274f66e
Maksym <maxim.yashyn@gmail.com>
2023-02-14 11:09:27 +0200

    implement PasswordValidationException exception class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        try {
+            password.equals(repeatPassword);
+        } catch (Exception e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
