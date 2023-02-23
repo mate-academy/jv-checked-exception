ef99e3a5da715c8767bff3247bd83d1234df3cd3
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-23 11:10:14 +0200

    fix spaces

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || password.length() < 10) {
-           throw new PasswordValidationException("Your passwords are incorrect. Try again.");
-        } else if (!(password.equals(repeatPassword))) {
-           throw new PasswordValidationException("Wrong passwords");
-        }
+            if (password == null || password.length() < 10) {
+                throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            } else if (!(password.equals(repeatPassword))) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
     }
 }
