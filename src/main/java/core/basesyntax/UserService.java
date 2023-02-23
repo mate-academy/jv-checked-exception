359a77bf61142bb2c2468b65a99c3b3a00db349f
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-23 11:33:18 +0200

    hw-jv-checked-exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
-public class UserService {
+public class UserService extends PasswordValidator {
     public void registerUser(User user) {
-        //write your code here
+        try {
+            validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
+
