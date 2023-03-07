45df6caff8cbfa6262c3b3b073c9a0cfee794afc
Victor Rudkovsky <vpcomtek@gmail.com>
2023-03-07 13:01:44 +0200

    HW jv-checked-exception v1.1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int VALID_PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+            if (password == null
+                    || password.length() < VALID_PASSWORD_LENGTH
+                    || !password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
