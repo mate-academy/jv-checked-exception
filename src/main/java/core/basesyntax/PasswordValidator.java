9776c48786b03fa26ce220cb8f5dacf354da8a6e
Andrii <subashandrii@gmail.com>
2023-03-10 11:54:48 +0200

    hw-jv-checked-exeption

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        //write your code here
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password != repeatPassword || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
