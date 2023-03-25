cfb0587a9b15a30794ad8bbd0c30be4e1eca94f4
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-03-25 21:40:50 +0100

    First try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
