e17403bc22bf48ec24709b41c59f4e1c327d3eb3
Nataliia Valiiaeva <nataliyavalyaeva1@gmail.com>
2023-03-26 17:34:47 +0300

    Fix

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class UserService extends PasswordValidator {
     public void registerUser(User user) {
         try {
-            validate(user.getPassword(), user.getRepeatPassword());
+            PasswordValidator passwordValidator = new PasswordValidator();
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException ex) {
             System.out.println("Your passwords are incorrect. Try again.");
