d9954b61b98f7a127a8cf6cf10d9cd7a1fa6f620
z1row <pmeysarosh2004@gmail.com>
2023-03-27 17:37:06 +0200

    Formatted properly

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,13 +2,13 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null ) {
+        if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
         if (password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (!password.equals(repeatPassword)){
+        if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
