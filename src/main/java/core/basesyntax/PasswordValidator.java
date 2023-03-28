a46ab9cbd911a577e45e92da8c6f62a896c8daac
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-28 15:32:01 +0300

    complete hw_v0.1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        try {
+            if(!(password.equals(repeatPassword)) || password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (Exception e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user){
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (Exception e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
