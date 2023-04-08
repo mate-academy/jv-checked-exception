1a59a1c96b155b089031eff69aad66e811cb136b
Stipaxa <petrivs572@gmail.com>
2023-04-08 13:14:34 +0300

    Created PasswordValidationException Implemented validate(String password, String repeatPassword) method and implemented registerUser(User user) method.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    private static final int SMALLEST_PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
+        if (password == null || repeatPassword == null
+                || password.length() < 10 || !(password.equals(repeatPassword))) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
