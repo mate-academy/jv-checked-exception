b504dfe32c97a9b7d46c96e687ebb952a4dab7f6
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-04-17 13:46:12 +0300

    delete empty line

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
-        if (password == null || repeatPassword == null || !password.equals(repeatPassword) || password.length() < 10) {
+        if (password == null || repeatPassword == null || !password.equals(repeatPassword)
+                || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
