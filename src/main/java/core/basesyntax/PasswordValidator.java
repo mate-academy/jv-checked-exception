24b07cb9fe5103b61ddbfe21640eaf6da2ecd01d
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-04-17 13:41:16 +0300

    move exception to if block

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,8 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
         if (password == null || repeatPassword == null || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
         }
-        throw new PasswordValidationException("Wrong passwords");
+
     }
 }
