e365ee8167751fee9a351c68a32bd53adc8bb071
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-19 19:13:04 +0200

    methods are written

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
 public class UserService {
+    private User someUser;
+    private PasswordValidator passwordValidator;
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+
     }
 
     public void saveUser(User user) {
