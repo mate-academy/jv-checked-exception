655e2678f9c480d54647373df3d205d586c2f25a
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-19 20:01:43 +0200

    6

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,10 +7,10 @@ public class UserService {
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
-
     }
 
     public void saveUser(User user) {
