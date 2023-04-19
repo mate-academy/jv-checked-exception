92665ca8c5ddbedadc2d62493a27d0970b0ea0cb
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-19 19:52:16 +0200

    added null check

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if ( password == null || repeatPassword == null ||!password.equals(repeatPassword) || password.length() < 10) {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null!");
+        }
+        if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
