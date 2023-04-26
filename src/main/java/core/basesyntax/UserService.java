108207b6e52f8acdb425f852d2307bc735f0acdc
Viktor Bykh <bykhviktor21@gmail.com>
2023-04-26 15:20:47 +0300

    first version of hw jv-checked-exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,22 @@
 package core.basesyntax;
 
 public class UserService {
+    private PasswordValidator passwordValidator;
+
+    public UserService(PasswordValidator passwordValidator) {
+        this.passwordValidator = passwordValidator;
+    }
+
     public void registerUser(User user) {
-        //write your code here
+        passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        } catch (NullPointerException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
