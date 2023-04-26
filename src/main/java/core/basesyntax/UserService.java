3920ae1fa74a1129acae739add63d5e3435d52e6
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-26 10:46:26 +0300

    exceptions were catched

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator pv = new PasswordValidator();
+    private final PasswordValidator pv = new PasswordValidator();
 
     public void registerUser(User user) {
         try {
