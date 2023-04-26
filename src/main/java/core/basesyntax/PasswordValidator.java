c901502aefb97a22adb2649280251fdf84e11e99
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-04-26 13:11:06 +0300

    Done tsk checked-exeption

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null && repeatPassword == null || !Objects.equals(password, repeatPassword)
+                || repeatPassword.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
