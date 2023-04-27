e9847fcbba51c3edb5257f22a0260716dc9f4cd5
Igor Maksimov <maksigors@gmail.com>
2023-04-27 09:07:01 +0300

    build succsess all well done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,10 +4,10 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again");
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (!repeatPassword.equals(password) || password.length() < 10) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again");
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
