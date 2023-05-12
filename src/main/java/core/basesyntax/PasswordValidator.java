731a223ed30c7d21d09becf2e13de39f416d8f40
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:28:33 +0300

    Update ci.yml to java 17

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
+        if ((password != null) && (password == repeatPassword) && (password.length() >= 10)) {
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
