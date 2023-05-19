5c64136110d4c661b429f458e35e1ba1345efd50
Mykhailo <mykhailokuryk16@gmail.com>
2023-05-19 18:56:44 +0300

    add solution after review

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -4,6 +4,4 @@ public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
-
-
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 public class UserService {
     private final PasswordValidator validator = new PasswordValidator();
 
-    public void registerUser(User user) throws PasswordValidationException {
-        validator.validate(user.getPassword(), user.getRepeatPassword());
-        saveUser(user);
-
+    public void registerUser(User user)  {
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
