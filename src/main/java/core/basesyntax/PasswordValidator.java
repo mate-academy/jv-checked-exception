053fa6e87878cdf808f394343b54589e19f01064
Andriy Karpa <McMerfee@i.ua>
2023-05-24 21:10:29 +0300

    do this test

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
         if (password != repeatPassword || password.length() < 10 || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
