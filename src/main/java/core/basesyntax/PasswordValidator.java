42694cf78b0ab129ef8204ed30b0984d73d00efa
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-05-26 15:45:47 +0300

    After mvn clean package

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,8 +5,8 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if(password == null || repeatPassword == null) {
-           throw new PasswordValidationException("Wrong passwords");
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (!password.equals(repeatPassword) || password.length() < PASSWORD_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
