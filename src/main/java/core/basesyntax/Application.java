ca3d28a417266e8aab3f828bc6cf9aed58b890bf
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-05-26 05:18:36 +0300

    added class Application and the method main. Called the method registerUser()

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,2 +1,9 @@
-package core.basesyntax;public class Application {
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) throws PasswordValidationException {
+        User user = new User("test@mail.com", "1234567890" , "1234567890");
+        UserService userService = new UserService();
+        userService.registerUser(user);
+    }
 }
