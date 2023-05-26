4572913ee55ffcca57d71bf9848ca6a5113ac781
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-05-26 05:16:46 +0300

    added called the method validate() and handled the exception. If everythinhg is ok, called the method saveUser(), if not print thr message Your passwords are incorrect. Try again

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    PasswordValidator passwordValidator = new PasswordValidator();
+    public void registerUser(User user) throws PasswordValidationException {
+       try {
+           passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+           saveUser(new User(user.getEmail(), user.getPassword(), user.getRepeatPassword()));
+       } catch (PasswordValidationException e){
+           throw new PasswordValidationException("Your passwords are incorrect. Try again");
+       }
     }
 
     public void saveUser(User user) {
