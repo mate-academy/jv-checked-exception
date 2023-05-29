f8400a54b25ff2a4ee53b8d6f217c4d888142578
OLYA <Ole4kastar79@gmail.com>
2023-05-29 23:17:17 +0300

    edit too long String ( method validate signature )

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException
-    {
-        if (!(password.equals(repeatPassword)) || (password.length() < 10)) {
+    public void validate(String password, String repPassword) throws PasswordValidationException {
+        if (!(password.equals(repPassword)) || (password.length() < 10)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
