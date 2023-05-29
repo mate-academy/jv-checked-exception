ffeb363249dd1f9b729272d82a9c7d6aaafa887c
OLYA <Ole4kastar79@gmail.com>
2023-05-29 23:42:45 +0300

    added null verification

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repPassword) throws PasswordValidationException {
-        if (!(password == null) && (password.length() > 0) && !(repPassword == null) && (repPassword.length() > 0)) {
+        if (!(password == null) && !(repPassword == null)) {
             if (!(password.equals(repPassword)) || (password.length() < 10)) {
                 throw new PasswordValidationException("Wrong passwords");
             }
