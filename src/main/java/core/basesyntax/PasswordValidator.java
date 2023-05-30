82af095ffe2853c7f7da8ae75fc1f86cc06fda05
OLYA <Ole4kastar79@gmail.com>
2023-05-30 22:52:25 +0300

    added catch NullPointerException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,12 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repPassword) throws PasswordValidationException {
-        if (!(password.equals(repPassword)) || (password.length() < 10)
-                || ((password == null) ^ (repPassword == null))) {
+        try {
+            if (!(password.equals(repPassword)) || (password.length() < 10)
+                       || ((password == null) ^ (repPassword == null))) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (NullPointerException e) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
