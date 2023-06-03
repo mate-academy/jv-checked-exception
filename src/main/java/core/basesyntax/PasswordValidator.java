921b01ff63e9c1f2bb3a34510743cc02bb290deb
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-06-03 22:36:47 +0300

    Version10-hw-Stas

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password,
-                         String repeatPassword)throws PasswordValidationException {
+                         String repeatPassword) throws PasswordValidationException {
         if (password == null || password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
