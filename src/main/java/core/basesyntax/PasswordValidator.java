fea58c99a109669a79e2c2af0b10cc6b32fcea4f
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-18 00:20:25 +0300

    Change access modifier and fix separators

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 public class PasswordValidator {
     private static final int MIN_PASSWORD_LENGTH = 10;
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null
-                || password != repeatPassword
-                || password.length() < MIN_PASSWORD_LENGTH) {
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null ||
+            password != repeatPassword ||
+            password.length() < MIN_PASSWORD_LENGTH) {
                 throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class UserService {
-    PasswordValidator passwordValidator = new PasswordValidator();
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
