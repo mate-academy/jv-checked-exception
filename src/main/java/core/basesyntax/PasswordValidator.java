6c5be4a687e493a5e28e3905ac6fd05abdc7397e
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-18 00:22:44 +0300

    Fix incorrect indentation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,9 +5,9 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null ||
-            password != repeatPassword ||
-            password.length() < MIN_PASSWORD_LENGTH) {
+        if (password == null || repeatPassword == null
+            || password != repeatPassword
+            || password.length() < MIN_PASSWORD_LENGTH) {
                 throw new PasswordValidationException("Wrong passwords");
         }
     }
