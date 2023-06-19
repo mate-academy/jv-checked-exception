516e7c77a3cb4d746c7a63a22825d465cfbd4f36
ANDREY <andrey.kiyas@gmail.com>
2023-06-19 15:37:53 +0300

    hw-jv-checked-exception-solution-andrey-kiyas (checkstyle)

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,7 +4,8 @@ public class UserService {
     private final PasswordValidator passwordValidator = new PasswordValidator();
 
     public void registerUser(User user) {
-        try {passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
