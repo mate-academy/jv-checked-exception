fa05d4442e6fdf83d1422feaa26830dd080df071
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-06-26 15:53:17 +0300

    added changes to the class UserService

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,16 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator pV;
+    private PasswordValidator passwordValidator;
 
     public UserService() {
-        this.pV = new PasswordValidator();
+        this.passwordValidator = new PasswordValidator();
     }
 
     public void registerUser(User user) {
         try {
-            pV.validate(user.getPassword(),
+            passwordValidator.validate(user.getPassword(),
                     user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
