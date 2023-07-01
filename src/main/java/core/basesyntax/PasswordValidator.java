21238a7447c65f7303075f1ab9e5263dc515700e
DaniilNepelev <kakashilvng@gmail.com>
2023-07-01 20:21:56 +0300

    solved issues with maven

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MIN_LENGTH_PASSWORD = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if(!(password.equals(repeatPassword)) || password.length() < MIN_LENGTH_PASSWORD) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (!(password.equals(repeatPassword)) || password.length() < MIN_LENGTH_PASSWORD) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
