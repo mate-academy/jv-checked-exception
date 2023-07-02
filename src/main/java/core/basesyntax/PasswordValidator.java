c692ed3d45debb0dcbb466863aa8cd896ecf2221
Mykola Horianin <anyavic2379@gmail.com>
2023-07-02 18:10:17 +0300

    Helped the user to register

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if ((password == null && repeatPassword == null)
+                || (password.length() == 0 && repeatPassword.length() == 0)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!repeatPassword.equals(password) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
