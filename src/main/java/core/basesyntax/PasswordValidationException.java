5d734f9a1bdce59bc232f5ac41530c6358db9ac3
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-03 12:58:30 +0300

    Password Exception and validator function

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,12 @@
 package core.basesyntax;
 
-//write your code here
+public class PasswordValidationException extends Exception {
+
+    public PasswordValidationException() {
+        super();
+    }
+
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
