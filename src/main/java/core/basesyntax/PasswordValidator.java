7a1333d3ce0f79430835c24cb6429a3770751841
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-04 17:43:03 +0300

    implemented user password validation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null");
+        }
+        if (password.length() < MIN_PASSWORD_LENGTH
+                && repeatPassword.length() < MIN_PASSWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
