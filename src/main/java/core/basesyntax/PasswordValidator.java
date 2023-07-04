fc9ec1d58b6c478bc64895834234355200b119f2
Artem Reizer <artem1707200290@gmail.com>
2023-07-04 18:37:51 +0300

    implemented methods registerUser and validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (!(password != null && password.equals(repeatPassword) && password.length() >= 10)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
-public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+public class UserService extends PasswordValidator {
+    public void registerUser(User user){
+        try {
+            validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
