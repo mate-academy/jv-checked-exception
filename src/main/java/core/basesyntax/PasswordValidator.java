b61102c7fdb83c1f576f7a5bcc27320844b8b16f
yermilov.m <yermilov.m@hotmail.com>
2023-07-06 23:22:21 +0200

    PasswordValidator class completed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,6 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || password.isEmpty()) {
+            throw new PasswordValidationException("Password shouldn't be empty");
+        } else if (repeatPassword == null || repeatPassword.isEmpty()) {
+            throw new PasswordValidationException("Repeat Password shouldn't be empty");
+        } else if (password.length() < 10 || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
