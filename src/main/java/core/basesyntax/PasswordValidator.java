84aea0fc43a201e3eb7b17e790cf5d687d5ef087
Oleh <popovich158@gmail.com>
2023-07-08 11:04:05 +0300

    Implemented registerUser method and validate method.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || password.isEmpty()) {
+            throw new PasswordValidationException("Wrong password");
+        } else if (password.length() < 10 && repeatPassword.length() < 10) {
+            throw new PasswordValidationException("Wrong password");
+        } else if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
     }
 }
