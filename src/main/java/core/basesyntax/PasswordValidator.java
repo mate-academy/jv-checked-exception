5ea0e7c02bc742ac227173cb0dd3ae4326e7e602
LTX <48835997+Lin-Rin@users.noreply.github.com>
2023-07-12 23:33:42 +0300

    task done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+
+    private static final String MESSAGE = "Your passwords are incorrect. Try again.";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException(MESSAGE);
+        }
+
+        if (password.length() < 10 && repeatPassword.length() < 10) {
+            throw new PasswordValidationException(MESSAGE);
+        }
+
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
+
 }
