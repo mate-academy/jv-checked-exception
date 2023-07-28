e3833b43c0d2887504ee6c5b62f9621bff00b487
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-07-28 22:13:19 +0300

    fixed errors causing test fails

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password.isBlank()) {
+        if (password == null || repeatPassword == null || password.isBlank()) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password.length() < 10 && password.length() != repeatPassword.length()) {
+        if (password.length() < 10 || password.length() != repeatPassword.length()) {
             throw new PasswordValidationException("Wrong passwords");
         }
         if (!password.equals(repeatPassword)) {
