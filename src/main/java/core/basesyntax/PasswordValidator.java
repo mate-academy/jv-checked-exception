f82d2cfb4d6aa68e15a56e21161f72fad4fee9ec
katym <katy.mozharovska@gmail.com>
2023-07-29 20:47:08 +0300

    required methods and exception created

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,12 +2,9 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public static void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null
-                || repeatPassword == null
-                || password.isEmpty()
-                || repeatPassword.isEmpty()
-                || password.length() < 10
-                || !password.equals(repeatPassword)) {
+        if (password == null || repeatPassword == null ||
+                password.isEmpty() || repeatPassword.isEmpty() ||
+                password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
