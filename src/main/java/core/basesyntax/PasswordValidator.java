b20e879641a0173a166ebf1ed00a458d47241086
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-05 00:55:13 +0300

    made line wrapping

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,13 +2,15 @@ package core.basesyntax;
 
 public class PasswordValidator {
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
 
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
 
-        if (password.equals(repeatPassword) && password.length() >= 10 && repeatPassword.length() >= 10) {
+        if (password.equals(repeatPassword) && password.length() >= 10
+                && repeatPassword.length() >= 10) {
         } else {
             throw new PasswordValidationException("Wrong passwords");
         }
