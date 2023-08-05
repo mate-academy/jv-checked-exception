488b4e2a6bf6aa757479cd6f95550a6c46fda4ae
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-05 15:33:35 +0300

    deleted commented code and add changed if blocks

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,11 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
 
-        if (password == null || repeatPassword == null) {
+        if (password == null || repeatPassword == null || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
 
-        if (password.equals(repeatPassword) && password.length() >= 10) {
-        } else {
+        if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
