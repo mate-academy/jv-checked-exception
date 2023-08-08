75a6eaa23390f9414d7ead535fc8503dc7e23363
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-08 17:30:14 +0300

    Implemented password validator.

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,12 @@
 package core.basesyntax;
 
+public class PasswordValidationException extends Exception{
+    public PasswordValidationException() {
+        super();
+    }
+
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
 //write your code here
