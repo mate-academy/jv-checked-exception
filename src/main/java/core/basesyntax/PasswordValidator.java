1e21c5f4a0ea8248a1ef876f673ba12747fec714
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-08 17:42:04 +0300

    Fixed typo p2.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException( "Wrong passwords");
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException( "Wrong passwords");
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (password.length() < 10 && repeatPassword.length() < 10) {
-            throw new PasswordValidationException( "Wrong passwords");
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
