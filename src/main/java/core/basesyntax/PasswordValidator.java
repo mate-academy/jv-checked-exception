e837dac353b8a4b139265ca56071f7d0290c1b56
Vyacheslav <torbin.official@gmail.com>
2023-08-14 14:30:46 +0300

    Test errors fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!repeatPassword.equals(password)
-                || password.length() < 10
-                || repeatPassword.length() < 10) {
-            throw new PasswordValidationException("PasswordValidationException : Wrong password");
+        if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator validator = new PasswordValidator();
     public void registerUser(User user) {
         try {
-            PasswordValidator passwordValidator = new PasswordValidator();
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
-        saveUser(user);
     }
 
     public void saveUser(User user) {
