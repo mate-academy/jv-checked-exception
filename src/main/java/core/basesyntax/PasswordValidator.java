2ca20c859dc04eef652c2efafb84cf548957de52
Pavlo Stefiniv <pastcwdt@gmail.com>
2023-08-16 03:44:02 +0300

    Password validating using the exceptions!

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        try {
+            if (password.length() < 10 || repeatPassword.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (password.length() == 0) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (!password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (password.matches("\\d+") || repeatPassword.matches("\\d+")) {
+                throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            }
+        } catch (NullPointerException e) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
+
