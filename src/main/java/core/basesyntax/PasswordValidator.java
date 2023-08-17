b9eb8c8a68b0bbcd360e1037a1709a8eb3d0ffde
Kostya Peresada <peresadakostya81@gmail.com>
2023-08-17 08:42:58 +0300

    all tests passed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        try {
+            if (password.length() < 10 || repeatPassword.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (password.length() == 0 || repeatPassword.length() == 0) {
+                throw new PasswordValidationException("Wrong passwords");
+            } else if (!password.equals(repeatPassword)) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (NullPointerException e) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 }
