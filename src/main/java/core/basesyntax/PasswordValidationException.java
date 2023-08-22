f22443f7054a17a22d031b1f2d40c51802a5c842
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-08-22 23:26:10 +0200

    HM done

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,9 @@
 package core.basesyntax;
+import java.io.IOException;
 
 //write your code here
+public class PasswordValidationException extends IOException {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
