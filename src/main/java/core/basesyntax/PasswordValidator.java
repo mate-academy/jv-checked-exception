e93bfd7f1b766fc71eba0bdd3479d69872be1bca
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-23 19:54:49 +0300

    Add implementation for the "validate" method.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+
+    private static final int REQUIRED_PASSWORD_LENGTH = 10;
+    private static final String WRONG_PASSWORD_MESSAGE = "Wrong passwords";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if ((password == null || repeatPassword == null)
+                || (password.length() < REQUIRED_PASSWORD_LENGTH
+                || repeatPassword.length() < REQUIRED_PASSWORD_LENGTH)
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException(WRONG_PASSWORD_MESSAGE);
+        }
     }
 }
