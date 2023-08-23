99033ab0bf60ef2533b2a2201733b80fcdbbbd2d
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-23 19:58:47 +0300

    Add implementation for the "register" method.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
+    private static final String INCORRECT_PASSWORDS_MESSAGE =
+            "Your passwords are incorrect. Try again.";
+
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println(INCORRECT_PASSWORDS_MESSAGE);
+            return;
+        }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
