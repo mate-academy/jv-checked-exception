a8f8a4abf60db8c18b56a97ec80fd31d1aa9f330
LeonIllya <dd0733567@gmail.com>
2023-09-06 18:55:02 +0300

    resolve maven

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
 
-        if (password.length() < 10 || repeatPassword.length() < 10 || password != repeatPassword) {
+        if (password.length() < 10 || repeatPassword.length() < 10 ||
+            !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
-
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -9,7 +9,7 @@ public class UserService {
             if (user.getPassword().equals(user.getRepeatPassword())) {
                 saveUser(user);
             }
-        } catch (RuntimeException | PasswordValidationException e) {
+        } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
