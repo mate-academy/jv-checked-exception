a2e900d6424823db36f47f5d546ee2c606eee283
Kseniia Stepanchenko <red.xantoria@gmail.com>
2023-09-10 17:31:53 +0300

    created methods in PasswordValidator, and PasswordValidationException

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
