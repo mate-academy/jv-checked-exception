f867f185bfea0eea067a3fcab02c5caf535e9d2f
Roman Menzheres <roma.menzheres@gmail.com>
2023-09-11 16:24:03 +0300

    Deleted unnecessary comment

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        // Decided to provide more info about error to help user understand what happened
+
         if (password == null
                 || password.length() < 10
                 || !password.equals(repeatPassword)) {
