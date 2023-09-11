29dbf928e56db43ced9bbc4a30e26889727a7e5c
Roman Menzheres <roma.menzheres@gmail.com>
2023-09-11 16:20:59 +0300

    Created own exception PasswordValidationException. Also added realisation for methods alidate to throw exception if user passwors are incorect and egisterUser to register user and handle exception if passwords are incorect

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         // Decided to provide more info about error to help user understand what happened
         if (password == null
                 || password.length() < 10
                 || !password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Wrong Password");
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
