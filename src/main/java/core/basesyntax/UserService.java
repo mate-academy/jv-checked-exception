4a806ed510163f9edd5b2ac045fea674c367f08a
Nazar <nazarmos13@gmail.com>
2023-09-20 09:08:49 +0300

    added catch NullPointerException

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,6 +5,8 @@ public class UserService {
         try {
             new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
+        } catch (NullPointerException e) {
+            System.out.println(true);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
