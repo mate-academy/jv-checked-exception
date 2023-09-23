b7712654eb96963a770b2b41331921c4a5f61793
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-23 16:10:16 +0200

    First solution of jv-checked-exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        saveUser(user);
     }
 
     public void saveUser(User user) {
