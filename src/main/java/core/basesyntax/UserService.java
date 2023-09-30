b568dc1f189dfbbf56ec1978c2109569b1cfab6a
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-09-30 22:17:54 +0300

    implemeted the registerUser() and validate() methods

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        UserService userService = new UserService();
+        try {
+            if (user.getPassword() != null && user.getRepeatPassword() != null) {
+                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+                userService.saveUser(user);
+            }
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
