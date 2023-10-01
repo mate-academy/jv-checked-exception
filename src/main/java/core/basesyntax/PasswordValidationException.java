327ef8c7e721017dd0eae1d467d0174d765b2d10
Hlib Kulikov <forzen123212@proton.me>
2023-10-01 22:33:25 +0300

    Realization of task

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,11 @@
 package core.basesyntax;
 
-//write your code here
+public class PasswordValidationException extends Exception {
+    PasswordValidationException() {
+        super();
+    }
+
+    PasswordValidationException(String message) {
+        super(message);
+    }
+}
