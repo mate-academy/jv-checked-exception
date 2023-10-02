db2366d5c55329c466052df0bd4caf73b2b83e3e
AdamMudrak <mudrets2000@gmail.com>
2023-10-02 21:09:47 +0300

    Implement validate method

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password.length() >= 10 && password.equals(repeatPassword)) {
+            return true;
+        } else {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 }
