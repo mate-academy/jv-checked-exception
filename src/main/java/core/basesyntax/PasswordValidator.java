9937c7c554aa060a4693d7102f75e7693ff35d39
AdamMudrak <mudrets2000@gmail.com>
2023-10-02 21:36:22 +0300

    Add null validation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password.length() >= 10 && password.equals(repeatPassword)) {
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if ((password != null && repeatPassword != null) && (password.length() >= 10)
+                && (password.equals(repeatPassword))) {
             return true;
         } else {
             throw new PasswordValidationException("Wrong passwords");
