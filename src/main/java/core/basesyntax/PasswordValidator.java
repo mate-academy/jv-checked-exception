daf361e99dfba3caa2e40fd4fe98c205356cb078
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-04 14:24:18 +0300

    implement registerUser() and validate()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MINIMUM_PASSWORD_LENGTH = 10;
+
+    public void validate(
+            String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null
+                || !password.equals(repeatPassword)
+                || password.length() < MINIMUM_PASSWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
