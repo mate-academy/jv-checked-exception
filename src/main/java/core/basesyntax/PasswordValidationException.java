3b3a28776977f3db088cac2723d8edc2291aa732
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-10 19:46:09 +0300

    all +

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,6 @@
 package core.basesyntax;
-
-//write your code here
+public class PasswordValidationException extends RuntimeException {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASS = 10;
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null ){
+            throw new PasswordValidationException("Input passwords.");
+        }
+        if (password.equals(repeatPassword) != true){
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (password.length() < MIN_PASS) {
+          throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
+        return;
     }
 }
