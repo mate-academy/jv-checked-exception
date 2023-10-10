1994ddf39deeb8cb7451867a773782be7923d876
Maksym Musiienko <maxymvm@gmail.com>
2023-10-10 11:07:26 +0300

    did customed exception for the password validation and validation logic as well

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Validation shouldn't throw NullPointerException for parameters: password - "
+                    + password + " and repeatPassword - " + repeatPassword + "\n");
+        }
+
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
