ff6a4beccd90d3eab382b4df5963272d7f05ea91
Illya <illios.piv@gmail.com>
2023-10-20 01:21:57 +0200

    Implement the methods 'registerUser()' in 'UserService' class and 'validate()' in 'PasswordValidator' class. Implement the class 'PasswordValidationException'.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
+// import com.sun.source.util.SourcePositions;
+
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        try {
+            new PasswordValidator().validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+        if (user.getPassword().length() >= 10
+                && user.getPassword().equals(user.getRepeatPassword())) {
+            this.saveUser(user);
+        }
     }
 
     public void saveUser(User user) {
