b8de74662cf81a7f04bb27b9d2c3f064b167a506
Vitalii Kazymyrovych <impero44@gmail.com>
2023-10-23 16:32:54 +0300

    solved task

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,17 @@
 package core.basesyntax;
 
 //write your code here
+public class PasswordValidationException extends Exception {
+
+    public PasswordValidationException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    /*public void throwExeption(String s1, String s2) {
+        if (!s1.equals(s2)
+                && ((s1.isEmpty() || s1.length() > 10)
+                || (s2.isEmpty() || s2.length() > 10))) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+    }*/
+}
