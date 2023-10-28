a18534bb191e52cefe78bef65b9e236e70d3de54
petro feniak <petro.fenyak@gmail.com>
2023-10-29 00:11:51 +0300

    implemented two methods registerUser() in UserService class and validate() in PasswordValidator class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
     }
 }
