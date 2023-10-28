1e2554694262df7788d156865d7ee3676e8cc78f
Bohdan <bohdan.g.54@gmail.com>
2023-10-29 00:22:16 +0300

    edited line that is longer than 100 symbols

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,9 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         int minPasswordLength = 10;
-        if (password != null && password.equals(repeatPassword) && password.length() >= minPasswordLength) {
+        if (password != null &&
+                password.equals(repeatPassword) &&
+                password.length() >= minPasswordLength) {
             return;
         }
         throw new PasswordValidationException("Wrong passwords");
