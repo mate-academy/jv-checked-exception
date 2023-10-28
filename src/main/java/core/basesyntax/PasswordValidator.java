dbd7425984ae4e3bb3c370e792c23e2a626feab5
petro feniak <petro.fenyak@gmail.com>
2023-10-29 00:22:09 +0300

    implemented two methods registerUser() in UserService class and validate() in PasswordValidator class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,5 +4,12 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         //write your code here
+        if (password != repeatPassword
+                || password == null
+                || repeatPassword == null
+                || password.length() < 10
+                || repeatPassword.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
