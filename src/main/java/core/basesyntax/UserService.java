df175dba57f8bc97041b50e7a1162812271a24ef
nazariykolesnikoww <nazarkolesnikov536@gmail.com>
2023-10-29 23:49:04 +0200

    commit #1

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,9 +2,14 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
-    }
+        PasswordValidator passwordValidator = new PasswordValidator();
 
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException | NullPointerException exception) {
+            System.out.println("Your password are incorrect. Try again!");
+        }
+    }
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
