9687f7f1a2cbc21ff1e9e99c9afde0072e19c5d2
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2023-10-31 01:17:30 +0200

    implementation method registerUser(), saveUser()

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.io.IOException;
+
 public class PasswordValidationException extends IOException {
     public PasswordValidationException(String message) {
         super(message);
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,15 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
+
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
-
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException | NullPointerException exception) {
-            System.out.println("Your password are incorrect. Try again!");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
+
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
