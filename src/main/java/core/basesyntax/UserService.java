889e04d7509d721a8ca8ea0873f4a775d7b41740
bobik <oliferchukrivne@i.ua>
2023-11-02 14:57:25 +0200

    task done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,17 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        if (user == null) {
+            System.out.println("User registration failed: User is null.");
+            return;
+        }
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
