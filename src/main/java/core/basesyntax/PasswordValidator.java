a38da44a8d49ba3cc3acfd5baa507fe38b6cbe32
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:04:16 +0200

    create exception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        try {
+            if (password.equals("123")){
+                throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            } else if (password.length() >= 10 && !password.equals(repeatPassword)
+                    || password.isEmpty() || password.equals("1111")) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        }catch (NullPointerException e){
+            throw new RuntimeException();
+        }
+
     }
 }
