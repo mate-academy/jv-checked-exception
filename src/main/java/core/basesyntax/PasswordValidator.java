594c180ed3814ff26d6e61a14798805b973d6d51
Dominik Gągało <cliffcamer@gmail.com>
2023-11-20 19:28:53 +0100

    Password validator style adjustment

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword)  throws PasswordValidationException {
-       if (password == null || !password.equals(repeatPassword) || password.length() < 10) {
-           throw new PasswordValidationException("Wrong passwords");
-       }
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+         if (password == null ||
+             !password.equals(repeatPassword) ||
+             password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+         }
     }
 }
