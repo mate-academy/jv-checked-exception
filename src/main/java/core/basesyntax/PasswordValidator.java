8031810d5ec7d5fc58243e8991c2460922f7f34f
Maksym Mazlov <mazlovmaxim@gmail.com>
2023-11-20 10:47:01 +0200

    implemented registerUser() and validate()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int MAX_PASSWORD_SIZE = 10;
+    public static final String MESSAGE_WRONG_PASSWORDS = "Wrong passwords";
+
+    public void validate(String password,
+                         String repeatPassword) throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException(MESSAGE_WRONG_PASSWORDS);
+        }
+        if (password.length() < MAX_PASSWORD_SIZE || repeatPassword.length() < MAX_PASSWORD_SIZE) {
+            throw new PasswordValidationException(MESSAGE_WRONG_PASSWORDS);
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException(MESSAGE_WRONG_PASSWORDS);
+        }
     }
 }
