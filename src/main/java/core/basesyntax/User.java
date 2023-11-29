70747d734f0d5bfb0b1a8e050720b000e024ac58
mikke966 <130836270+mikke966@users.noreply.github.com>
2023-11-29 21:04:24 +0100

    added solution

--- src/main/java/core/basesyntax/User.java
+++ src/main/java/core/basesyntax/User.java
@@ -4,6 +4,7 @@ public class User {
     private String email;
     private String password;
     private String repeatPassword;
+
     public User(String email, String password, String repeatPassword) {
         this.email = email;
         this.password = password;
