a702c8f9d819d0935e5dcf4d69f471aedad341af
Igor Hunder <gunderigorv@gmail.com>
2023-12-10 22:04:04 +0200

    I made exceptions

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int DATA_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null");
+        }
+        if (password.length() < DATA_LENGTH || repeatPassword.length() < DATA_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!password.matches(".*[A-Z].*") || !password.matches(".*[a-z].*")) {
+            throw new PasswordValidationException("Passwords are not the same");
+        }
     }
 }
