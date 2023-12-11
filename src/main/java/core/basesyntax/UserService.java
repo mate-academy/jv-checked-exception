92881becef8ffeaff7906fb5e51be76cd4d7d3ef
Valentyn Sharshon <woosay333@gmail.com>
2023-12-11 14:06:54 +0200

    created the 'PasswordValidator' field, implemented the 'registerUser()' method, which checks the user's data and displays an informational message if the data is not valid

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import static java.lang.System.out;
+
 public class UserService {
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException ex) {
+            out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
