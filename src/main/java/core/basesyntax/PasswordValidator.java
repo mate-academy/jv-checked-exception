e9483302a7160342fd415d1941a7bd76ac3c615a
Kate892F <tophours13@gmail.com>
2023-12-12 09:03:52 +0200

    refactor code

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (Objects.isNull(password) || Objects.isNull(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (!password.equals(repeatPassword) || password.length() < 10 || repeatPassword.length() < 10) {
+        if (!password.equals(repeatPassword) || password.length() < 10
+                || repeatPassword.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
