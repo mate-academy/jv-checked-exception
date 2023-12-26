b651028fc23c68f99c5fd60b6f40ac71284949ae
Anton <StarAntonU@gmail.com>
2023-12-27 01:49:28 +0200

    implement method validate, registerUser and create Exception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final String MESSAGE = "Wrong passwords";
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null && repeatPassword == null
+                || password.length() < 10 && repeatPassword.length() < 10
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException(MESSAGE);
+        }
+
     }
 }
