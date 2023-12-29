cff5df32946e8bb9fb0ff89f53ecaf443750f781
NekitCool1 <gnnikita97@gmail.com>
2023-12-29 18:56:51 +0200

    Method validate(), Method registerUser(), PasswordValidationException class were implemented

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) throws PasswordValidationException {
-        User user = new User("fedsaf", null, null);
-        UserService userService = new UserService();
-        userService.registerUser(user);
-    }
-}
