4ea60956dc3c17b3c8ba2ea906cb493ad199bab0
Artem <artem.kozakevych@gmail.com>
2023-12-30 13:32:46 +0200

    release PasswordValidator

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,15 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = user.getPassword();
+        String repeatPassword = user.getRepeatPassword();
+        try {
+            passwordValidator.validate(password, repeatPassword);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+        saveUser(user);
     }
 
     public void saveUser(User user) {
