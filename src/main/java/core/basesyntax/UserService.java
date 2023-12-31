aa0d6b8fb6d15216b5e879ed18a29f1796e62b7d
John <pandastars535@gmail.com>
2023-12-31 03:40:34 +0200

    did MVN clean Package

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class UserService {
-     private final PasswordValidator passwordValidator = new PasswordValidator();
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(),user.getRepeatPassword());
@@ -10,7 +11,6 @@ public class UserService {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
-
 
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
