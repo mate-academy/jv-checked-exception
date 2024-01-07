8dd995c3c0dbbad5258a51f15927a95855353c83
Alex Naumenko <alex_naumenko_@ukr.net>
2024-01-08 00:08:25 +0200

    created PasswordValidationException class and implemented it implemented validate and registerUser methods

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    private PasswordValidator validator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
     public void saveUser(User user) {
