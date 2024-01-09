02916ac83df91046f8bf9ab3b75996cf3fabe7cc
MolchanovAlexander <rybkolhoz@gmail.com>
2024-01-09 15:39:22 +0200

    task-is-completed-first-commit

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,9 @@
 package core.basesyntax;
 
 //write your code here
+public class PasswordValidationException extends Exception {
+    //create your exception User with given email doesn't exist
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
+
     public void registerUser(User user) {
         //write your code here
+        PasswordValidator p = new PasswordValidator();
+        try {
+            if (p.validate(user.getPassword(), user.getRepeatPassword())) {
+                saveUser(user);
+            }
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
