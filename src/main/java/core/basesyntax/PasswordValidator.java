622f92cc3e13d23b5e7c4864dfe820147349edce
Marysabal <mary@Mary-MacBook-Pro.local>
2024-01-10 16:55:06 +0200

    is done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,16 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+            System.out.println("User registered successfully.");
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again. Reason: "
+                    + e.getMessage());
+        }
     }
 
     public void saveUser(User user) {
