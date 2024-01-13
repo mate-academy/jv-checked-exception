3f3554fef635b3a342c55c0fe35f7fd515487684
Kate Drevilo <rabmargo@gmail.com>
2024-01-13 12:56:02 +0200

    Added create PasswordValidationException and implement methods registerUser and validate.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    static final int MIN_PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (!password.equals(repeatPassword) || password.length() <= MIN_PASSWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+            System.out.println("registration successful");
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString()
+                + " was saved to database!!!");
     }
 }
