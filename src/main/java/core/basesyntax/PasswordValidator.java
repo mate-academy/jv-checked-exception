cc7997cd03cce0e69a1e9289da9094e430e9fd6d
Dmytro Nedashkovskyi <dmitrynedashkovsky@gmail.com>
2024-01-25 02:52:08 +0200

    reg

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static void validate(String email, String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Input data cannot be null");
+        }
+
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,34 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+
+    private List<User> userList = new ArrayList<>();
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            PasswordValidator.validate(user.getEmail(), user.getPassword(),
+                    user.getRepeatPassword());
+            saveUser(user);
+            System.out.println("User " + user.toString() + " was saved to database!!!");
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        try {
+            PasswordValidator.validate(user.getEmail(), user.getPassword(),
+                    user.getRepeatPassword());
+            userList.add(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 }
