c6ba01236a0a027459969c9b4e5c93ffce4e3629
Dariia <daradyradara@gmail.com>
2024-01-27 18:46:49 +0200

    Created own checked exception in PasswordValidationException.java, check password and repeat password in PasswordValidator.java in the method validate, in UserService.java have been written registerUser() with try catch check

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        try{
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e){
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
