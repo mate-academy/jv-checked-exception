906cf421c1cf611c41c4c2542aa0bd1d49dac539
Dariia <daradyradara@gmail.com>
2024-01-28 17:13:33 +0200

    Fixed checkstyle and logic of method validate()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,14 +1,20 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    private static final int PASSWORD_LENGTH = 10;
+    private static final int MIN_PASSWORD_LENGTH = 10;
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() > PASSWORD_LENGTH
-                    || repeatPassword.length() > PASSWORD_LENGTH
-                    || !password.equals(repeatPassword)) {
-                throw new PasswordValidationException("Wrong passwords");
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
         }
+
+        if (password.length() < MIN_PASSWORD_LENGTH
+                    || repeatPassword.length() < MIN_PASSWORD_LENGTH
+                    || !password.equals(repeatPassword)
+                    || password.isEmpty()) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
     }
 }
