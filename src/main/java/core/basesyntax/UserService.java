8b647f7a6a0c278d879dc437586e6d0c0dafc6a9
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-01-29 22:41:48 +0200

    Solved problem with validation and registartion of users

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
+    private boolean isValid = false;
+    private PasswordValidator validator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            isValid = true;
+        } catch (PasswordValidationException a) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+        if (isValid) {
+            saveUser(user);
+        }
     }
 
     public void saveUser(User user) {
