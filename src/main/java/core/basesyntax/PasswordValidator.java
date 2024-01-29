347933520f1a8fe53eacec5cf35ed077e8b167b0
Kacper <kacperkukula97@gmail.com>
2024-01-29 21:32:13 +0100

    First Solution, added custom checked exception.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final String ERROR_MESSAGE = "Wrong passwords";
+    private static final int MIN_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
+        if (password == null
+                || repeatPassword == null
+                || password.length() < MIN_LENGTH
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException(ERROR_MESSAGE);
+        }
     }
 }
