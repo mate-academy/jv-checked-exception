7153bbf00590e228feed0ffb07b7a5049e862768
rk0v <rz20@ukr.net>
2024-02-03 20:23:39 +0200

    implements registerUser()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int VALID_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if ((password.length() < VALID_PASSWORD_LENGTH) && (password.length() != repeatPassword.length())) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if ((password == null) || !(password.equals(repeatPassword))
+                || (password.length() < VALID_PASSWORD_LENGTH)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
