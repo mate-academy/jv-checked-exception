831c3627fca32367a50de66bf955fd66fd2390e0
HAIOVIY <gaevoywork@gmail.com>
2024-02-07 19:23:16 +0200

    add white space in the end because File does not end with a newlin, changed validate method with new else if for null.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        if (!password.equals(repeatPassword) || password.length() < 10
-                || password == null || repeatPassword == null) {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
