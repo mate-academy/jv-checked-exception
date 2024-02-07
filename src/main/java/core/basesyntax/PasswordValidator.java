df0de59d6080c1fba3d0bc6cfffc25018734aa0c
HAIOVIY <gaevoywork@gmail.com>
2024-02-07 19:12:41 +0200

    fix small mistake with used 'if' without {}

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        if (!password.equals(repeatPassword) || password.length() < 10)
+        if (!password.equals(repeatPassword) || password.length() < 10
+                || password == null || repeatPassword == null ) {
             throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
