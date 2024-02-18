e1a2fe9cbec1115a1cb790667a4c0c5deb0be4e7
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-18 17:27:31 +0200

    codestyle fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,15 +1,16 @@
 package core.basesyntax;
-import core.basesyntax.PasswordValidationException;
 
 public class PasswordValidator {
     private static final String EXCEPTION_MESSAGE = "Wrong passwords";
     private static final int MIN_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null ||
-                                password.length() < MIN_PASSWORD_LENGTH ||
-                                repeatPassword.length() < MIN_PASSWORD_LENGTH ||
-                                !password.equals(repeatPassword)) {
+    public void validate(String password,
+                         String repeatPassword) throws PasswordValidationException {
+
+        if (password == null || repeatPassword == null
+                             || password.length() < MIN_PASSWORD_LENGTH
+                             || repeatPassword.length() < MIN_PASSWORD_LENGTH
+                             || !password.equals(repeatPassword)) {
             throw new PasswordValidationException(EXCEPTION_MESSAGE);
         }
     }
