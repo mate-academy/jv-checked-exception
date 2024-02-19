8b098dc052fa72b58e1d7b6aff6c3fa0ed185d7a
Oksana Miazina <oksana.myazina@gmail.com>
2024-02-19 18:17:27 +0200

    feat: add user registration

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.print("Your passwords are incorrect. Try again.");
+            return;
+        }
+
+        saveUser(user);
     }
 
     public void saveUser(User user) {
