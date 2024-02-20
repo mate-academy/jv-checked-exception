aa7c6e850c7f8e9dd2a03cdd89462b43c148a817
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-20 13:13:20 +0200

    PasswordValidationException.java - Override method message PasswordValidator.java - if\else added for validation check UserService.java - try catch with PasswordValidationException created

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    // User user = new User("sas","ss","ss");
+    PasswordValidator check = new PasswordValidator();
     public void registerUser(User user) {
-        //write your code here
+       try {
+           check.validate(user.getPassword(), user.getRepeatPassword());
+           saveUser(user);
+       } catch (PasswordValidationException e) {
+           System.out.println("Your passwords are incorrect. Try again.");
+       }
     }
 
     public void saveUser(User user) {
