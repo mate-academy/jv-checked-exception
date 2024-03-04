9bf38f561c9ee75bae50bc310b8ea582bad60c06
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-04 12:13:32 +0200

    Finished task

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() < 10 ) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException
+    {
+        if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         } else if (password == null && repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
