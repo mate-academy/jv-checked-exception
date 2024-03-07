92778c0ce0f8af859a0c744a0ff1986d537b5691
Banderan <bartoszwojcik2004@gmail.com>
2024-03-07 17:17:50 +0100

    Filling registerUser method

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,6 +6,7 @@ public class UserService {
         //write your code here
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
