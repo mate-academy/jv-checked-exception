54b7d1c9e0d290f81bc1ef782fd8d715b8bf67e3
Banderan <bartoszwojcik2004@gmail.com>
2024-03-07 23:08:38 +0100

    Code after mvn clean package

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class UserService {
-    PasswordValidator passwordValidator = new PasswordValidator();
+    private static PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
