455c7b526e00358af7b446d7a3f14da62d09aa1b
Banderan <bartoszwojcik2004@gmail.com>
2024-03-07 23:00:10 +0100

    Finish writing code with checking testClass

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!((password.equals(repeatPassword)) && (password.length() >= 10) && (repeatPassword.length() >= 10))) {
+        if (password == null || repeatPassword == null || !((password.equals(repeatPassword)) && (password.length() >= 10) && (repeatPassword.length() >= 10))) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
