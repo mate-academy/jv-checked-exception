f752de835c2be9f80f61b40b7154e62f68f77df7
Banderan <bartoszwojcik2004@gmail.com>
2024-03-07 23:25:36 +0100

    Trying solve mvn problems

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
+
         super(message);
     }
 }
