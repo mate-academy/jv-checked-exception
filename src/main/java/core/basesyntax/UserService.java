3895d3c11925fd9b8c12784ac941bf0961bf9f13
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-03-13 15:58:30 +0200

    Update UserService.java

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,9 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator passwordValidator;
+
+    public UserService() {
+        this.passwordValidator = new PasswordValidator();
+    }
+    
     public void registerUser(User user) {
         try {
-            PasswordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
