baa2fb415dc707272c853800ef8243116be7a78d
1 <glebpashko2004@gmail.com>
2024-03-16 16:57:15 +0200

    Implemented methods registerUser(), validate().

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MINIMUM_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword)) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || (password.length() < MINIMUM_PASSWORD_LENGTH)
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password.length() < MINIMUM_PASSWORD_LENGTH) {
-            throw new PasswordValidationException("Password length is less than 10");
-        }
+
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 public class UserService {
     private final PasswordValidator passwordValidator = new PasswordValidator();
 
-    public void registerUser(User user) throws PasswordValidationException {
-        passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
-        saveUser(user);
+    public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
