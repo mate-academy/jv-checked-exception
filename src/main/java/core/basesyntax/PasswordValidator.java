d3220100f49c294727ac56aa57b1ee3d3f406bec
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-16 18:09:04 +0200

    Validating Password for nullable, valid length, equality to repeat password

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASSWORD_LENGTH = 10;
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || !checkIsValidPasswordWithLength(password)  || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+    }
+
+    private boolean checkIsValidPasswordWithLength(String password) {
+        return password.length() >= MIN_PASSWORD_LENGTH;
     }
 }
