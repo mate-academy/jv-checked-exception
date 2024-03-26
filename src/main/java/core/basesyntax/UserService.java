13a9a37d70934790be019cf49ee326ff868cc8ae
slavik33 <slavik33a@gmail.com>
2024-03-26 12:49:16 +0200

    допоможіть виправити

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,23 @@
 package core.basesyntax;
 
 public class UserService {
+
+    private final PasswordValidator passwordValidator;
+
+    public UserService() {
+        this.passwordValidator = new PasswordValidator();
+    }
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            if (user.getPassword() == null || user.getRepeatPassword() == null) {
+                throw new PasswordValidationException("Password or repeatPassword is null");
+            }
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
