21a42d3e254d949fb9bd3787787830a2780967c6
slavik33 <slavik33a@gmail.com>
2024-03-26 12:49:16 +0200

    допоможіть виправити

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
-    public PasswordValidationException() {
-        super();
-    }
 
-    public PasswordValidationException(String massage) {
-        super(massage);
+    public PasswordValidationException(String message) {
+        super(message);
     }
 }
+
+
+
