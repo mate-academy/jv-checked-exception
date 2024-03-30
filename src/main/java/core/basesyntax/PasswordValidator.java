20a4f7af4cb893e17d0b6697d677832de03a2944
Vladyslav Fushtor <v.fushtor@gmail.com>
2024-03-30 04:40:18 +0200

    tack solved

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException
-    {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password.length() >= 10 && repeatPassword.equals(password)) {
             return;
         }
