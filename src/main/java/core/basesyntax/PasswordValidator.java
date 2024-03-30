03af00b9dca3ed0d4bce738e4482c675709d9f4b
Vladyslav Fushtor <v.fushtor@gmail.com>
2024-03-30 22:05:16 +0200

    fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException, NullPointerException {
+        if (password == null || repeatPassword == null) {
+            throw new NullPointerException("Input data cannot be null");
+        }
         if (password.length() >= 10 && repeatPassword.equals(password)) {
             return;
         }
