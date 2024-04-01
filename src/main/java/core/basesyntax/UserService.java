d78818598c4e65c3a7142323baf6b8028878e971
Svitlana Galicka <svitlanakozak12332@gmail.com>
2024-04-01 19:24:56 +0300

    implemented methods

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException exception) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
