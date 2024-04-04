2768092eedc357b8ef093d4b018466a75cb9c23c
slavik33 <slavik33a@gmail.com>
2024-04-04 10:43:10 +0300

    виправив помилки

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -9,3 +9,4 @@ public class PasswordValidationException extends Exception {
         super(massage);
     }
 }
+
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -16,3 +16,4 @@ public class UserService {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
+
