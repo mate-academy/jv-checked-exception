26d3cdba0411060717a58ada51a56ec9e33ad928
flamehost <flamehost@syneforge.com>
2024-04-17 22:45:12 +0300

    display edits

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null
-                || !password.equals(repeatPassword)
-                || password.length() < 10) {
+    public void validate(String passwrd, String repeatPasswrd) throws PasswordValidationException {
+        if (passwrd == null || !passwrd.equals(repeatPasswrd) || passwrd.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,7 +6,7 @@ public class UserService {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        } catch (PasswordValidationException e ) {
+        } catch(PasswordValidationException e ) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
