270bf00ec1bd02bf2447f8a24d5acd8826d58098
Oleksii Deineka <deineka219@gmail.com>
2024-04-18 17:48:46 +0300

    implement methods validate and registerUser according to given task

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,8 @@
 package core.basesyntax;
+public class PasswordValidationException extends Exception {
 
-//write your code here
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
+
