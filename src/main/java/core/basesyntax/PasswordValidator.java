9ed48d9a09d3af954d8b7ea1604f9aca3042b4e8
Eugene <eugene.taranenko.it@gmail.com>
2024-04-20 01:43:17 +0300

    maven's errors was fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int PASS_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if ((password == null || repeatPassword == null)
                 || (password.length() < PASS_LENGTH || repeatPassword.length() < PASS_LENGTH)
                 || !password.equals(repeatPassword)) {
