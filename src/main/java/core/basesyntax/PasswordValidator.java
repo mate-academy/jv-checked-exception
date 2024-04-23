74a13a4c2e251eb1e5742444caaefd0dcab5d636
Sofiia Boiko <boikosofiiawork@gmail.com>
2024-04-23 14:48:29 +0300

    implement registerUser and validate methods. create a PasswordValidationException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password,
+                         String repeatPassword) throws PasswordValidationException {
+        if (password == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else if (password.length() < 10) {
+            throw new PasswordValidationException(
+                    "Your password should be longer than 10 characters!"
+            );
+        }
     }
 }
