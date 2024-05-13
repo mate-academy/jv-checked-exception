c148bf9eb7aafc64c14ae53bf2c16b108ae45391
Andrii Riezin <andrey.o.rezin@gmail.com>
2024-05-13 16:04:07 +0300

    solved due to task

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MAX_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Password or repeatPassword cannot be null");
+        }
+        if (!password.equals(repeatPassword) || password.length() < MAX_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
