e694b8c8d17b75c25a627931ca5d22ab147d9a7f
Tymofii Snopov <snop.0904@gmail.com>
2024-05-14 16:20:24 +0200

    done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    private PasswordValidator passwordValidator;
+
+    public UserService() {
+        this.passwordValidator = new PasswordValidator();
+    }
+
+    public void registerUser(User user, String password, String repeatPassword) {
+        try {
+            passwordValidator.validate(password, repeatPassword);
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
