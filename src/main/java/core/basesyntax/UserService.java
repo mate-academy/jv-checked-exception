6c34651f4370001ca72c10e08db52aa88e840ac3
Greshch <greshchook@gmail.com>
2024-05-15 18:58:19 +0300

    fix catch line

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,8 +7,7 @@ public class UserService {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        }
-        catch (PasswordValidationException e) {
+        } catch (PasswordValidationException e) {
             System.out.println(e.getMessage());
         }
     }
