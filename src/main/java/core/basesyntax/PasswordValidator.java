73c965697354f6df694dd49db07411c5e3d582f3
Greshch <greshchook@gmail.com>
2024-05-17 08:24:07 +0300

    clear throws in validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public static final int MIN_REQUARED_PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword)
-            throws PasswordValidationException {
+    public void validate(String password, String repeatPassword) {
         if (password == null || repeatPassword == null
                 || password.length() < MIN_REQUARED_PASSWORD_LENGTH
                 || repeatPassword.length() < MIN_REQUARED_PASSWORD_LENGTH
