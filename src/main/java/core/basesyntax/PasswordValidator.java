cade5799f81fb6fa28fa5e76e87ea732d10a7568
smagles <lesialist@icloud.com>
2024-05-20 17:42:44 +0200

    Add validation for password - null and repeatPassword - null

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() < 10 || !password.equals(repeatPassword)) {
+        if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong passwords");
         }
-        if (password == null || repeatPassword == null) {
+        if (password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
