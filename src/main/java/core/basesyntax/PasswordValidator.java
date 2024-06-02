74a4486f9cd2409a4552506ba73bbdee471041cd
Oleksii <obabych1@stu.vistula.edu.pl>
2024-06-02 14:58:52 +0200

    fixed mistake with longer characters

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
