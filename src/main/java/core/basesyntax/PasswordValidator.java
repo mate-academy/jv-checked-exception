314663c22b3cdf156dbf11d446cccaf4590dcd34
Oleksii <obabych1@stu.vistula.edu.pl>
2024-06-02 15:13:06 +0200

    fixed mistake

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || !password.equals(repeatPassword)) {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
