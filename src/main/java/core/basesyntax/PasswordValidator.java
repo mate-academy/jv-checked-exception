475e6eae1086691c13c22b98b102a7af30214d9a
Wersebu <cz.damax@gmail.com>
2024-06-04 11:58:37 +0200

    All tests passed ready for check..

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,9 +5,9 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
 
-        if(password != null) {
-            if(password.equals(repeatPassword)) {
-                if(password.length() >= 10) {
+        if (password != null) {
+            if (password.equals(repeatPassword)) {
+                if (password.length() >= 10) {
                     new UserService();
                 } else {
                     throw new PasswordValidationException
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class UserService {
 
     private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
