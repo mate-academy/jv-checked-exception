8fabf3a9cc5d7cab15944479f20ac4c40a32a67c
Tymofii Snopov <snop.0904@gmail.com>
2024-06-10 15:23:35 +0200

    done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || password.isEmpty() ||
-                repeatPassword == null || repeatPassword.isEmpty() ||
-                !password.equals(repeatPassword) ||
-                password.length() < 10) {
+        if (password == null || password.isEmpty()
+                || repeatPassword == null || repeatPassword.isEmpty()
+                || !password.equals(repeatPassword)
+                || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
