e154096a4c742dbf46f667749a69d7c0d453d8a7
Panchenko Dmytro <panchenko0795@gmail.com>
2024-06-16 17:26:41 +0300

    Implemented methods: `registerUser()` in UserService class, `validate()` in PasswordValidator class

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MIN_PASSWORD_SIZE = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null
+                || password.isEmpty() || repeatPassword.isEmpty()
+                || password.length() < MIN_PASSWORD_SIZE || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
