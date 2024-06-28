294efe3cbefe0c914cd0b43313b3a1191344c51e
Anton Pekariev <litsss30071989@gmail.com>
2024-06-28 19:33:19 +0300

    the checked-exception task is complete

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
+        if ((password == null) || (password == null) || (password.length() < 10) || (repeatPassword.length() < 10)
+            || (!password.equals(repeatPassword))) {
+                throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
