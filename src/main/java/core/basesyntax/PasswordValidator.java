7d8da9cce6fb669d9e6144800cf4002273e81290
FakeJavaLearner <miriman@ukr.net>
2024-07-05 11:55:26 +0200

    code style improvement

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,7 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null
-        || !password.equals(repeatPassword) || password.length() < MIN_PASSWORD_LENGTH) {
+                || !password.equals(repeatPassword) || password.length() < MIN_PASSWORD_LENGTH) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
