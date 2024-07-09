b6dbf9ca847f763f3ad40806e256cc6688c165fe
KLICKICE <ledmaskiight@gmail.com>
2024-07-09 12:22:44 +0300

    Test failure

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int PASSWORD_MAX_LENGTH = 10;
+
+    public boolean validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Password or repeatPassword cannot be null");
+        }
+
+        if (password.equals(repeatPassword) && password.length() >= PASSWORD_MAX_LENGTH) {
+            System.out.println("Successful");
+            return true;
+        }
+
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
