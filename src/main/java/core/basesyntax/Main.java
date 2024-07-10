992ef69d59555e7d84f89d25c6f64d72a0c08082
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-10 23:59:56 +0300

    Changed the main a little

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         UserService service = new UserService();
-        User user = new User("vakosovij@gmail.com","1234124","1234567890");
-        service.registerUser(user);
+        User firstUser = new User("vakosovij@gmail.com","1234124","1234567890");
+        service.registerUser(firstUser);
+        User secondUser = new User("vakosovij@gmail.com","12345678910","12345678910");
+        service.registerUser(secondUser);
     }
 }
