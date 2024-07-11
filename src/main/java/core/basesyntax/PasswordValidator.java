d716b8e92b0843523b15d2258ff14b05f9f24e0d
Yehor Nazarets <nazarets1111@gmail.com>
2024-07-11 21:27:17 +0200

    added PasswordValidator and exception methods

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public static final int MIN_VALUE_FOR_PASSWORD = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password != repeatPassword || password.length() < MIN_VALUE_FOR_PASSWORD
+                || password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
