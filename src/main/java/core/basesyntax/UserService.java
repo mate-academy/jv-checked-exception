7d4daa9a53f2d6bcb2c83025c535af3c372ecb23
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-07-27 10:31:07 +0200

    4th attempt

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,32 +2,32 @@ package core.basesyntax;
 
 public class UserService {
 
-    public void registerUser(User user) {
-        try {
-            passwordValidate(user.getPassword(), user.getRepeatPassword());
-            saveUser(user);
-            System.out.println("User " + user + " was saved to database!!!");
-        } catch (PasswordValidationException e) {
-            System.out.println("Your passwords are incorrect. Try again.");
-        }
-    }
 
     public void passwordValidate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
+
+          if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Passwords cannot be null");
         }
+
+
         if (password.isEmpty() || repeatPassword.isEmpty()) {
             throw new PasswordValidationException("Passwords cannot be empty");
         }
+
         if (password.length() < 6) {
-            throw new PasswordValidationException("Passwords must be at least 6 characters long");
+            throw new PasswordValidationException("Password is too short");
         }
+
+
         if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Passwords do not match");
         }
     }
 
-    private void saveUser(User user) {
+    public void saveUser(User user) {
+        System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
+
+
