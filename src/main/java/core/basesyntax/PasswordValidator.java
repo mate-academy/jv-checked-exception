7e9c308222a54d5a79b5f65090a8559b69a473c0
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-07-27 09:42:37 +0200

    1 st attempt

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,5 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
         //write your code here
+        if (!password.equals(repeatPassword) || password.length() >= 10) {
+            throw new IllegalArgumentException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,10 +2,17 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator validator = new PasswordValidator();
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Wrong passwords");
+        }
     }
 
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
+
