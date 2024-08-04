b6a7b464a77ba3807b89d662a9f6e801df6e9818
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2024-08-04 14:53:07 +0300

    created a password checker, if everything is correct - the user will be saved

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public static void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public static void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Passwords cannot be null");
         }
-        if (repeatPassword.length() < 10 || password.length() < 10 || !repeatPassword.equals(password)) {
+        if (repeatPassword.length() < 10 || password.length() < 10 ||
+                !repeatPassword.equals(password)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,7 +4,7 @@ public class UserService {
     public void registerUser(User user) {
         try {
             PasswordValidator.validate(user.getPassword(), user.getRepeatPassword());
-            saveUser (user);
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
