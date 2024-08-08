702976756ffe833cab06a17680ebf7e05490775d
Oleh <olegvyrvykhvost@gmail.com>
2024-08-08 11:02:06 +0300

    completed homework jv-checked-exception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int INCORRECT_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword)
+                || password.length() < INCORRECT_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
