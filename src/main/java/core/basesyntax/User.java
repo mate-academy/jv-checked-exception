1d0525b5e128fbb940db89db8c5bf01776553dbf
Dima Davydov <Kilo6559@gmail.com>
2024-08-14 16:27:23 +0300

    Work complete

--- src/main/java/core/basesyntax/User.java
+++ src/main/java/core/basesyntax/User.java
@@ -12,26 +12,32 @@ public class User {
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
     public String getRepeatPassword() {
+
         return repeatPassword;
     }
 
     public void setRepeatPassword(String repeatPassword) {
+
         this.repeatPassword = repeatPassword;
     }
 
