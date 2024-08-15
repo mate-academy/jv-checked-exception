97ec1b9b5d389be5c903333a8a714fe461eca4ab
Dmytro <dimadukalis@gmail.com>
2024-08-15 15:54:09 +0300

    program for registration

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int PASSWORD_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Password cannot be null");
+        }
+        if (!password.equals(repeatPassword) || password.length() < PASSWORD_LENGTH
+                && repeatPassword.length() < PASSWORD_LENGTH) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
