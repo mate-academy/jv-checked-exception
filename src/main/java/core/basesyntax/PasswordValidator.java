37d301ad14df2d55304dc6921f44121cd8e3c644
StoneBlood <brkalix@gmail.com>
2024-08-26 13:36:55 +0300

    implemented method validate

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-
+        if (password == null || repeatPassword == null ||
+                !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
