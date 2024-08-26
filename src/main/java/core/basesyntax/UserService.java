b185dd6f48c394a49bb6ca0460d09b8a24224d72
StoneBlood <brkalix@gmail.com>
2024-08-26 13:51:58 +0300

    fixed code style errors

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
-public class UserService {
-    public void registerUser(User user) {
+public class UserService extends PasswordValidator {
+    private PasswordValidator passwordValidator;
 
+    public UserService() {
+        this.passwordValidator = new PasswordValidator();
+    }
+
+    public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
