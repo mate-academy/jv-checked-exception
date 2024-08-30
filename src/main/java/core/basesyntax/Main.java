02e8217a7ef5f4aa95aca37d128971709b8b5a02
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-30 13:06:30 +0200

    I wrote a program due to the task and created one more method in order to see how does this program work

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class Main {
+
     public static void main(String[] args) {
         User user = new User("nikolya.cr@gmail.com", "123456789110", "12345678910");
         UserService service = new UserService();
