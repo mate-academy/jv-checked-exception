9724d8a1dabbef2138bf0f94370a3ae2d506a57f
Illia Kulyk <kulykillia12@gmail.com>
2024-09-11 15:48:19 +0300

    Login user

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null");
+        }
         if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
