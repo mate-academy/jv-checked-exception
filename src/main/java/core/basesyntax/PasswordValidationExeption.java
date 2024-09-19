28d592213075c7f277d6464f9db5f78a5279b7c1
Mateusz Seler <mateusz.seler@gmail.com>
2024-09-19 13:14:20 +0200

    first version - something goes wrong and I have no idea what

---
+++ src/main/java/core/basesyntax/PasswordValidationExeption.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class PasswordValidationExeption extends Exception {
+    public PasswordValidationExeption(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,10 +2,22 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationExeption e) {
+            System.out.println("Unfortuntly something goes rong. "
+                    + "Check if the emails are the same and have at least 10 characters");
+            return;
+        }
+
+        saveUser(user);
     }
 
     public void saveUser(User user) {
+
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
