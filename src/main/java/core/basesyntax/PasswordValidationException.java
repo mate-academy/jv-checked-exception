4b575729047a34ffe7e8977560465b150aadb212
Anastasiia Dihich <an.digich@gmail.com>
2024-09-22 17:40:33 +0300

    checkstyle maven

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
-    public class PasswordValidationException extends Exception {
+
+public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
 }
-
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class UserService {
-    PasswordValidator passwordValidator = new PasswordValidator();
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
