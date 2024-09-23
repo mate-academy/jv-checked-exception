39aae9e5bc08277141227690b40b27d0f0bcc06a
Roman <romanvoynahiy@gmail.com>
2024-09-23 18:30:12 +0300

    Implement user registration functionality: - Added PasswordValidationException. - Created validate() method in PasswordValidator. - Updated registerUser() to handle validation and save user.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        boolean isPasswordMatch = password.equals(repeatPassword);
+
+        if (!isPasswordMatch || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
