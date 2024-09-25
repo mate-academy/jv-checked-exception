77e12d35fbcf43232d0ee9817d509b1ec68829d1
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-25 22:34:47 +0300

    complited the assigment

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,15 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        try {
+            PasswordValidator passwordValidator = new PasswordValidator();
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            User newUser = new User(user.getEmail(), user.getPassword(), user.getRepeatPassword());
+            saveUser(newUser);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
+
     }
 
     public void saveUser(User user) {
