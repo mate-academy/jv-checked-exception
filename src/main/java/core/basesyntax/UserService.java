689dddeb3d81fc5a991eb9f1a32327e980e4582c
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-09-30 23:39:50 +0100

    completion of all tasks

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator checkPassword = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            checkPassword.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
