16e6d3de191b22329cf36cd0ece779873990aefa
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-09 10:42:56 +0200

    Update PasswordValidator.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,9 @@
 package core.basesyntax;
 
-public class PasswordValidator {
-    public static final int MIN_PASSWORD_LENGTH = 10;
-
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null 
-            || !password.equals(repeatPassword) 
-            || password.length() < MIN_PASSWORD_LENGTH) {
-            throw new PasswordValidationException("Wrong passwords");
-        }
+public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    if (password == null || repeatPassword == null
+            || password.length() < MIN_PASSWORD_LENGTH
+            || !password.equals(repeatPassword)) {
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
