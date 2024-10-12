d45ecfd3010db64e465ac4008c3e5ec479e48383
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-12 21:40:20 +0200

    Delete src/main/java/core/basesyntax/PasswordValidationException.java

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class PasswordValidationException extends Exception {
-    
-    public PasswordValidationException(String message) {
-        super(message);
-    }
-}
-
