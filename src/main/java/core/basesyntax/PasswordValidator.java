386358d847885edb0c85dce44108f7456cec5360
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-15 09:34:04 +0200

    PasswordValidator.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,19 +1,6 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public static final int MIN_PASSWORD_LENGTH = 10;
-
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Password cannot be null");
-        }
-
-        if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Passwords do not match");
-        }
-
-        if (password.length() < MIN_PASSWORD_LENGTH) {
-            throw new PasswordValidationException("Password is too short");
-        }
+    public void validate(String password, String repeatPassword) {
+        //write your code here
     }
-}
