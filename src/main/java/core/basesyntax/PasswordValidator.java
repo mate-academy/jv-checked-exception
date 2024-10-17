58e5ad1744c0c5a14ec889de497c88af1359ea8d
Dymchenko, Mykola <mdymchen@estee.com>
2024-10-17 10:53:17 -0500

    Solution for the task jv-checked-exception: - added implementation to classes: PasswordValidationException, PasswordValidator, and UserService

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public boolean validate(String password,
+                            String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        } else {
+            return true;
+        }
     }
 }
