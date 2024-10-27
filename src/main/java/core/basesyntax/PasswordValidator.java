39e0cba2b01e5e97e589af84be7e6e8de04e569f
Sasha Koval <kovalsana40@gmail.com>
2024-10-27 22:18:08 +0200

    It`s a good try1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,7 +6,9 @@ public class PasswordValidator {
     public String getMessage() {
         return message;
     }
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+
         //write your code here
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Your passwords are incorrect. Try again.");
