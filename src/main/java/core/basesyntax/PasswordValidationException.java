833bab82848b425e54c406fad8088321d2e9b2d1
Volikkk <illavolanuk02@gmail.com>
2024-10-27 13:16:05 +0200

    add2

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -4,4 +4,4 @@ public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
-}//write your code here
+} //write your code here
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,6 +4,6 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
-        }//write your code here
+        } //write your code here
     }
 }
