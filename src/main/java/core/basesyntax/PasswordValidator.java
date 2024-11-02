9906b90f75733c97b20d17a7236216e1fbde34fc
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-02 20:15:05 +0100

    refactor code style2

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        if (!password.equals(repeatPassword) && password.toCharArray().length < 10) {
-            throw new PasswordValidationException();
+        if (!password.equals(repeatPassword) || password.toCharArray().length < 10) {
+            throw new PasswordValidationException("Wrong password");
         }
     }
 }
