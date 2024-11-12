5062ded8791dc9309c9ded66462f2c6481f70a1a
Roman Leshchyshyn <romchuk1969@gmail.com>
2024-11-12 09:54:53 +0300

    ee

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class PasswordValidator extends Exception {
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
-
-
 }
+
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,20 +5,14 @@ public class UserService {
 
     public void registerUser(String email, String password, String repeatPassword) {
         try {
-
             passwordValidator.validate(password, repeatPassword);
-
-
             saveUser(email, password);
             System.out.println("User registered successfully!");
         } catch (PasswordValidationException e) {
-
             System.out.println("Your passwords are incorrect. Try again.");
         }
     }
-
     private void saveUser(String email, String password) {
-
         System.out.println("User with email " + email + " saved to database.");
     }
 }
