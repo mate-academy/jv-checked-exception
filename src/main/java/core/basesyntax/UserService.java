889df4511e4ebb1f079c3f8718e1601614b74572
Roman Leshchyshyn <romchuk1969@gmail.com>
2024-11-12 09:51:13 +0300

    ee

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,24 @@
 package core.basesyntax;
 
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
+    public void registerUser(String email, String password, String repeatPassword) {
+        try {
+
+            passwordValidator.validate(password, repeatPassword);
+
+
+            saveUser(email, password);
+            System.out.println("User registered successfully!");
+        } catch (PasswordValidationException e) {
+
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
-    public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+    private void saveUser(String email, String password) {
+
+        System.out.println("User with email " + email + " saved to database.");
     }
 }
