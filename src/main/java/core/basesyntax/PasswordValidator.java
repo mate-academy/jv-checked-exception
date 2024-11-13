de323ec5cc938167fbf9db12f6ece26856ee773e
Teslenko Daniil <burgalhashem@gmail.com>
2024-11-13 14:36:08 +0200

    maked user service

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
-        //write your code here
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong password");
+        }
     }
 }
