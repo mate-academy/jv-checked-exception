217c6d8ffa8acfa54736db138fb1925249392f16
Serhii Slobodianiuk <sergey.slobodianiuk@gmail.com>
2024-11-13 19:29:17 +0200

    issues are fixed

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() >= 10) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        boolean isPasswordNull = password == null || repeatPassword == null;
+
+        if (isPasswordNull || !password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
