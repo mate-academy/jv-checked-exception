4077954fe352e5a746f71c38e624f0accc2ae163
Teslenko Daniil <burgalhashem@gmail.com>
2024-11-13 15:01:00 +0200

    maked user service

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator passwordValidator= new PasswordValidator();
+    private PasswordValidator validator= new PasswordValidator();
 
     public void registerUser(User user) {
         try {
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            validator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
