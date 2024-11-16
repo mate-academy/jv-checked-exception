0619ffa56144c7486cfbd12bbb7d41cb55fbca6b
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-11-17 00:21:08 +0100

    Update PasswordValidator2.java

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,16 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) 
+    public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if ((password == null || repeatPassword == null)
-             || (passwordlength() < 10 || repeatPassword.length() < 10)
-             || !(password.equals(repeatPassword))) {
-
-         throw new PasswordValidationException ("Wrong passwords");
-       }
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (password.length() < 10 || repeatPassword.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
