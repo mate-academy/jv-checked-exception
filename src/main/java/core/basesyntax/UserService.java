c007502dc3b83ccbcb6ba305bb757aec5c41b63f
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-11-17 00:39:17 +0100

    Update CheckStyle2UserService.java

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,9 +6,8 @@ public class UserService {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-
         } catch (PasswordValidationException e) {
-            System.out.println("Your password are incorrect. Try again.");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
 
@@ -16,3 +15,4 @@ public class UserService {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
 }
+
