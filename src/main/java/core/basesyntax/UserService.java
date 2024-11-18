144ad53cc0e6a605cb0c0e1f8e0cdc46e20dcc7b
Roman Leshchyshyn <romchuk1969@gmail.com>
2024-11-18 17:59:20 +0300

    eq

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class UserService {
-    private PasswordValidator passwordValidator = new PasswordValidator();
+    private final PasswordValidator passwordValidator = new PasswordValidator();
 
     public void registerUser(User user) {
         try {
