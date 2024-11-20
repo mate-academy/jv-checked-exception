38d52fee18821dfe77705752f09d556bfed3ec48
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-20 18:02:22 +0000

    3

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,7 +4,7 @@ public class UserService {
     private final PasswordValidator passwordValidator = new PasswordValidator();
     public void registerUser(User user) {  
         try {
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword()) 
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword()); 
             user.saveUser();
         } catch(PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
