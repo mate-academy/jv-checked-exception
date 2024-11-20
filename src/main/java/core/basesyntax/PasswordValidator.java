eaf437b75e54ccde618421915ce58a1bf19b5538
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-20 19:08:28 +0000

    3992984

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,13 +4,13 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword) 
             throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Паролі не можуть бути null");
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (password.length() < 10 || repeatPassword.length() < 10) {
-            throw new PasswordValidationException("Паролі повинні містити щонайменше 10 символів");
+            throw new PasswordValidationException("Wrong passwords");
         }
         if (password.equals(repeatPassword) == false) {
-            throw new PasswordValidationException("Паролі не співпадають");
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
