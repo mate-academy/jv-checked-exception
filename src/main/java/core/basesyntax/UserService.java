fe0937317dccde114a204668ef5c17f351401d0f
Rafal <pyszkowski.r@gmail.com>
2024-11-24 14:19:02 +0100

    second

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
-import javax.xml.validation.Validator;
-
 public class UserService {
-    public void registerUser(User user)  {
-       try {
-           user.validate(user.getPassword(), user.getRepeatPassword());
-           saveUser(user);
-       } catch (PasswordValidationException e) {
-           System.out.println("Your passwords are incorrect. Try again.");
-       }
+    public void registerUser(User user) {
+        try {
+            user.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
+
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
     }
