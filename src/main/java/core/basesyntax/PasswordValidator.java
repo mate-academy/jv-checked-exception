1eebb18df1a724364fb2d61bb12c545ad95d909e
Bohdan_SSX <mr.dimov77@gmail.com>
2024-12-04 19:28:18 +0200

    fixed line 101 symbol

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || !Objects.equals(password, repeatPassword) || password.length() < 10) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null
+                || !Objects.equals(password, repeatPassword)
+                || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
