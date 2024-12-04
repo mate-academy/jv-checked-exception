e99c1421d768b0e3839083db5f465bc2602058a0
Bohdan_SSX <mr.dimov77@gmail.com>
2024-12-04 19:25:40 +0200

    Added password validation

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,24 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
         System.out.println("User " + user.toString() + " was saved to database!!!");
+    }
+
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        User user = new User("dawd@ga.co", "123124123124", "123124123124");
+        userService.registerUser(user);
     }
 }
