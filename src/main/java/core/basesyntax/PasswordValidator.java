98d87ee43e779602363d352b30abc3caa50ab504
Mizuo <mizuo.personal@gmail.com>
2025-01-05 22:45:09 +0100

    ...

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
         try {
             if (password.length() >= 10 && password.equals(repeatPassword)) {
@@ -9,7 +10,7 @@ public class PasswordValidator {
             } else {
                 throw new PasswordValidationException("Wrong passwords");
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
