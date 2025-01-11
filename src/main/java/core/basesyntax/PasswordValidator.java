6a894941d93cefc7d79465701c007a78a438b098
denDy1224 <denchik12327@gmail.com>
2025-01-11 01:38:03 +0100

    fixed_check_style_exception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword
+    ) throws PasswordValidationException {
         if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException ("Wrong passwords");
+            throw new PasswordValidationException("Wrong passwords");
         }
 
         if (password.length() >= 10) {
-            throw new PasswordValidationException ("Password must be at least 10 characters long.");
+            throw new PasswordValidationException("Password must be at least 10 characters long.");
         }
     }
 }
