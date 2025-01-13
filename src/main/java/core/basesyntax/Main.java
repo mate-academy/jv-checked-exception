73974cd5fbfb5efd759b2c58dc17986a75d38147
Julia <jjuliabudny@gmail.com>
2025-01-13 23:34:22 +0100

    solution 3

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,15 +5,19 @@ public class Main {
         UserService userService = new UserService();
 
         //Poprawne hasła
-        User user1 = new User("email1","CorrectPassword123", "CorrectPassword123");
+        User user1 = new User("email4","CorrectPassword123", "CorrectPassword123");
         userService.registerUser(user1);
 
         //Niepoprawne hasła
-        User user2 = new User("email2","short", "short");
+        User user2 = new User("email5","short", "short");
         userService.registerUser(user2);
 
         //Hasła niezgodne
-        User user3 = new User("email3","CorrectPassword123", "DifferentPassword123");
+        User user3 = new User("email6","CorrectPassword123", "DifferentPassword123");
         userService.registerUser(user3);
+
+        //Hasła null
+        User user4 = new User("email7",null, null);
+        userService.registerUser(user4);
     }
 }
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public static void validate(String password, String repeatPassword)
             throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null");
+        }
         if (password.equals(repeatPassword) && password.length() >= 10) {
-            System.out.println("Matching Passwords");
             return;
         }
         throw new PasswordValidationException("Wrong passwords");
