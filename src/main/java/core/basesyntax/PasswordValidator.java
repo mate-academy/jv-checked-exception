111a4557253504633d99dc080e18820827428b21
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-15 21:34:43 +0200

    PasswordValidatorException class created

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,5 +3,6 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) {
         //write your code here
+
     }
 }
---
+++ src/main/java/core/basesyntax/PasswordValidatorException.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class PasswordValidatorException extends Exception {
+    public void passwordValidatorException (String message) {
+        System.out.println(message);
+    }
+}
+
