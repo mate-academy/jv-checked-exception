09f7bea33ba033223a07624a3f10bdcde5c25958
anton <anton.surzhok@gmail.com>
2025-01-26 14:11:49 +0200

    Fixed Checkstyle issues in PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,9 +2,13 @@ package core.basesyntax;
 
 // Клас для перевірки паролів
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         // Перевірка паролів
-        if (password == null || repeatPassword == null || password.length() < 10 || !password.equals(repeatPassword)) {
+        if (password == null
+                || repeatPassword == null
+                || password.length() < 10
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Incorrect passwords");
         }
     }
