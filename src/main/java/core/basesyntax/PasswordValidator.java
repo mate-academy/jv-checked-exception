b8103ca4217fb29093845b022cb0c0ab66108fe6
anton <anton.surzhok@gmail.com>
2025-01-26 14:42:50 +0200

    Moved PasswordValidationException and PasswordValidator to separate files

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,15 +1,13 @@
 package core.basesyntax;
 
-// Клас для перевірки паролів
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        // Перевірка паролів
         if (password == null
                 || repeatPassword == null
                 || password.length() < 10
                 || !password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Incorrect passwords");
+            throw new PasswordValidationException("Wrong passwords"); // Змінено текст
         }
     }
 }
