11cf9a66f7e44ec1a5e7f6e75df0dd28782a2d3f
Nazar <nazar0302@gmail.com>
2025-02-05 22:17:10 +0200

    add whitespaces

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password.equals(repeatPassword) && password.length() >= 10) {
             System.out.println("Your password is correctly!");
         }
