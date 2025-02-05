19ce242668b46788c26aea8ee9261c2468a07616
Nazar <nazar0302@gmail.com>
2025-02-05 21:36:59 +0200

    implement registerUser(), validate() methods

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
+import java.net.PasswordAuthentication;
+
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    public void registerUser(User user) throws PasswordValidationException {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        if (user != null) {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        }
     }
 
-    public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+    public void saveUser(User user) throws PasswordValidationException {
+        if (user.getRepeatPassword().equals(user.getPassword())
+                && user.getRepeatPassword().length() >= 10) {
+            System.out.println("User " + user.toString() + " was saved to database!!!");
+        }
+        throw new PasswordValidationException("Wrong passwords");
     }
 }
