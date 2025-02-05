bff39961b277be55af159723d5cf1968f357cdb0
Nazar <nazar0302@gmail.com>
2025-02-05 22:21:18 +0200

    1111111111

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,7 +5,8 @@ public class PasswordValidator {
             throws PasswordValidationException {
         if (password.equals(repeatPassword) && password.length() >= 10) {
             System.out.println("Your password is correctly!");
+        } else {
+            throw new PasswordValidationException("Wrong passwords");
         }
-        throw new PasswordValidationException("Wrong passwords");
     }
 }
