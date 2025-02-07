a64a96bbc39f4d8cd3efecc313fc75afef145c8d
Artem Hurievskyi <gurievsky.artem@gmail.com>
2025-02-07 19:25:04 +0200

    use checked exception to check passwords

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
+    private PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
         //write your code here
     }
 
