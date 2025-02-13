6ac50432da89cd9773abbf26ca5920d7dce37a57
Mateusz <mateusz.kiryk@o2.pl>
2025-02-13 11:41:39 +0100

    First solution

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) {
+        User validUser = new User("test@example.com", "securepassword123"
+                , "securepassword123");
+        User invalidUser = new User("user@example.com", "short", "short");
+
+        UserService userService = new UserService();
+
+        System.out.println("Registering valid user:");
+        userService.registerUser(validUser);
+        System.out.println("\nRegistering invalid user:");
+        userService.registerUser(invalidUser);
+    }
+}
