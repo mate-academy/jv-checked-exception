17e9805400fe7e51fc47b72dfc9f7cb23b3fda02
Mateusz <mateusz.kiryk@o2.pl>
2025-02-13 11:55:14 +0100

    Third solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -11,7 +11,6 @@ public class Application {
         System.out.println("Registering valid user:");
         userService.registerUser(validUser);
 
-        User invalidUser = new User("user@example.com", "short", "short");
         System.out.println("\nRegistering invalid user:");
         userService.registerUser(invalidUser);
     }
