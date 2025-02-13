f82638c39ecae30e0bbcb0393da66dbbb8b0d37c
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-02-13 01:03:25 +0100

    Fixed PR issues and removed old comments

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -12,7 +12,7 @@ public class UserService {
 
         }
     }
-        public void saveUser (User user){
+        public void saveUser (User user) {
             System.out.println("User " + user.toString() + " was saved to database!!!");
         }
     }
