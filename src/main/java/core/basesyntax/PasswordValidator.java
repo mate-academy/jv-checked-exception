cab7003460814609d4cd010ba9d49dded8a6568b
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-14 12:54:56 +0100

    checkedsolution.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Passwords do not match.");
