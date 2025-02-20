4f2e48692e4de4677deeeb8c2ed4d9e24ace2c33
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-20 14:39:37 +0100

    jvexception

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+    //write your code here
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         if (password == null || repeatPassword == null
