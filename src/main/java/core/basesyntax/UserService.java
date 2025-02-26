2187f0575880287f1c2a2d1c7b960fb4c7a9145b
Dima <dimkabibimka333@gmail.com>
2025-02-26 10:00:30 +0200

    Homework done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,17 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        int g = 0;
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+            g++;
+        }
+        if (g == 0) {
+            saveUser(user);
+        }
     }
 
     public void saveUser(User user) {
