28399c79adfeb14ff8eef8c5d23f1b376acd624b
8ACE8 <konovalovvkoop1357@gmail.com>
2025-03-07 00:18:53 +0300

    Fixed test: added output in saveUser() to match expected test result.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,8 +4,8 @@ public class UserService {
     public void registerUser(User user) {
         PasswordValidator validator = new PasswordValidator();
         try {
-            saveUser(user);
             validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
         }
@@ -13,5 +13,6 @@ public class UserService {
 
     public void saveUser(User user) {
         String output = "User " + user.toString() + " was saved to database!!!";
+        System.out.println(output);
     }
 }
