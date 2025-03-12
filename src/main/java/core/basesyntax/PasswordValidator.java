1a590f0ca4be8920582116945e2fbe942e9952b0
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-12 19:18:37 +0200

    created exception, validate() method implemented

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (!isPasswordCorrect(password) || !isPasswordCorrect(repeatPassword)
+                || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+    }
+
+    public boolean isPasswordCorrect(String password) {
+        return (password != null && password.length() >= 10);
     }
 }
