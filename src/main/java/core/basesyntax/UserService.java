b8b6df35878caede7965d5f0587171fa44bdc549
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-03-13 17:02:23 +0200

    Update UserService.java

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,6 +6,6 @@ public class UserService {
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " was deleted!!!");
     }
 }
