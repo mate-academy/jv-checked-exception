a3218f0c2397a70e859ebc3845cae06b155513dd
HSlawinski <hubi.slawinski@gmail.com>
2025-03-14 21:10:54 +0100

    Fixed NullPointerException

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,9 +4,11 @@ public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
-            passwordValidator.validate(user.getPassword(),
-                    user.getRepeatPassword());
-            saveUser(user);
+            if (user.getPassword() != null && user.getRepeatPassword() != null) {
+                passwordValidator.validate(user.getPassword(),
+                        user.getRepeatPassword());
+                saveUser(user);
+            } else throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
             throw new PasswordValidationException("Your passwords are incorrect. Try again.");
