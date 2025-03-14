a0c8ee998c1f451391eed51ea03c138b7f7b3ce0
HSlawinski <hubi.slawinski@gmail.com>
2025-03-14 20:58:19 +0100

    Added exception, validator and registerUser

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,10 +2,18 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(),
+                    user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
-
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString()
+                + " was saved to database!!!");
     }
 }
