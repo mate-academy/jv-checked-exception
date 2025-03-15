ea92fd1e513ae75d755f01741cbaddca773c6f87
Alexandros <olek4004@gmail.com>
2025-03-15 17:48:57 +0100

    checked exception fix

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,9 +1,8 @@
 package core.basesyntax;
 
-public class PasswordValidationException extends RuntimeException {
+public class PasswordValidationException extends Exception {
 
   public PasswordValidationException(String message) {
     super(message);
-
   }
 }
