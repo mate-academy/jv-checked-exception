7df88be9a9841c3484be14e458a7233babf8f39a
Alexandros <olek4004@gmail.com>
2025-03-15 17:52:57 +0100

    indentation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,8 +2,11 @@ package core.basesyntax;
 
 public class PasswordValidator {
 
-    public static void validate(String password, String repeatPassword) throws PasswordValidationException {
-
+    public static void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Passwords cannot be null");
+        }
         if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
