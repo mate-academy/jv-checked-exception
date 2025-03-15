9426a54d78e591bfd247b71f45ac6f0e8764deeb
HSlawinski <hubi.slawinski@gmail.com>
2025-03-15 16:16:00 +0100

    Done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,16 +4,15 @@ public class UserService {
     public void registerUser(User user) {
         PasswordValidator passwordValidator = new PasswordValidator();
         try {
-            if (user.getPassword() != null && user.getRepeatPassword() != null) {
-                passwordValidator.validate(user.getPassword(),
-                        user.getRepeatPassword());
-                saveUser(user);
-            } else {
+            if (user.getPassword() == null || user.getRepeatPassword() == null) {
                 throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            } else if (user.getPassword() != null || user.getRepeatPassword() != null) {
+            passwordValidator.validate(user.getPassword(),
+                    user.getRepeatPassword());
+            saveUser(user);
             }
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
         }
     }
 
