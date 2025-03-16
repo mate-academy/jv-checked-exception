57c6381c675e7000b146eace612bffa808a49d1f
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-16 19:39:39 +0100

    second  try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public static void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!(password.equals(repeatPassword)) && (password.length() < 10) || password.equals("null"))
+    public static void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (!(password.equals(repeatPassword)) && (password.length() < 10)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
-
+}
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,10 +5,8 @@ public class UserService {
         try {
             PasswordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
-        }
-        catch (PasswordValidationException e) {
+        } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
-
         }
     }
 
