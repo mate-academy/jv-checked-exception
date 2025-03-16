38c5e033d928b69db47c2c94ea973b5332b019f6
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-16 19:57:04 +0100

    third try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 public class PasswordValidator {
     public static void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!(password.equals(repeatPassword)) && (password.length() < 10)) {
+        if ((password == null) || (repeatPassword == null)) {
             throw new PasswordValidationException("Wrong passwords");
         }
+        if (!(password.equals(repeatPassword)) || (password.length() < 10)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
     }
 }
