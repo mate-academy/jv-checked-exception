ac43025c88edf850079b2f3df1a35220753d6d68
Nazarii Vypor <vypor25102006@gmail.com>
2025-03-19 20:31:53 +0200

    Виправлено помилку з NullPointerException у PasswordValidator
    - Додано перевірку на null у методі validate()

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 class PasswordValidator {
     public static void validate(String password, String repeatPassword)
             throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Password cannot be null");
+        }
         if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
+
 
