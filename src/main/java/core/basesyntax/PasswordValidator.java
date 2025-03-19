906da020b0ea0369a6797f172c6e883a24b1af49
Nazarii Vypor <vypor25102006@gmail.com>
2025-03-19 20:25:40 +0200

    - Відформатовано метод validate для відповідності ліміту в 100 символів

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 class PasswordValidator {
-    public static void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public static void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (!password.equals(repeatPassword) || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
         }
