aa2a544d7bbe09425bc441a95ba3f7a8eb43224a
Vershynin Kostia <bigvini@gmail.com>
2025-03-24 22:18:29 +0200

    implement user registration with password validation

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null
                 || password.length() == 0 || repeatPassword.length() == 0
                 || password.length() < 10 || repeatPassword.length() < 10
