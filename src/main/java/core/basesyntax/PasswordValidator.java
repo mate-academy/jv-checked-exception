34103da0bc35a974ddad0b75cbd6ac2d73024aa6
Slawomir <slawomir_pacek@op.pl>
2025-03-26 16:40:36 +0100

    exception do sprawdzenia5

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,9 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         //write your code here
-        if (password.equals(repeatPassword) && password.length() >= 10 && password != null) {
+        if (password == null || repeatPassword == null) {
+            System.out.println("Password shouldn't be empty");
+        } else if (password.equals(repeatPassword) && password.length() >= 10) {
             System.out.println("Password is valid");
         } else {
             throw new PasswordValidationException("Wrong passwords");
