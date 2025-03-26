09e9f8a7b79530041bace744744f97beb63d80e1
Slawomir <slawomir_pacek@op.pl>
2025-03-26 16:24:08 +0100

    exception do sprawdzenia

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,15 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        PasswordValidator PassValid = new PasswordValidator();
+
+        try {
+            PassValid.validate(user.getPassword(), user.getRepeatPassword());
+        } catch (PasswordValidationException e) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
+
+        saveUser(user);
     }
 
     public void saveUser(User user) {
