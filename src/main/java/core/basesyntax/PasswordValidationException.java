be17398940863b2d4b7eabd89e5f02117565db97
Dmytro Shkliarenko <dmitry.shklyarenko@gmail.com>
2025-04-03 18:38:41 +0300

    adding logic functional

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,7 @@
 package core.basesyntax;
-
 //write your code here
+public class PasswordValidationException extends Exception {
+    PasswordValidationException (String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+    public static void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
+        if(password == null || repeatPassword == null || !password.equals(repeatPassword) || password.length() >= 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
