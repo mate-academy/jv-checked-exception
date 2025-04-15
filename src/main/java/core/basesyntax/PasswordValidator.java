2f16018453dd92c243db8c5a80a90b7c3689455e
User <n0938951561@gmail.com>
2025-04-15 13:40:25 +0200

    complete task and mvn check

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int MAX_LENGHT = 10;
 
-    public void validate(String password, String repeatPassword) throws
-            PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() > MAX_LENGHT
-                || repeatPassword == null) {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (repeatPassword == null || password.length() < MAX_LENGHT
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
