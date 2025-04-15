480a37d018ba00ba8d5f293a6d88a6c01d8bf8ef
User <n0938951561@gmail.com>
2025-04-15 13:33:27 +0200

    make validate and registerUser but cant past all test

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final int MAX_LENGHT = 10;
+
+    public void validate(String password, String repeatPassword) throws
+            PasswordValidationException {
+        if (!password.equals(repeatPassword) || password.length() > MAX_LENGHT
+                || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
