e0ec48443a55e179621e5cd41f77079577bc6963
Mirrox-cloud <beznosmaksim0102@gmail.com>
2025-04-20 19:52:30 +0300

    3 attempt

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -2,10 +2,7 @@ package core.basesyntax;
 
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Password and password are required");
-        }
-        if (!password.equals(repeatPassword) || password.length() < 10) {
+        if (password == null || repeatPassword == null || password.length() < 10 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
