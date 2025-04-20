c426688b75231dc357b71232c172761ce5920fb3
Mirrox-cloud <beznosmaksim0102@gmail.com>
2025-04-20 19:46:51 +0300

    solved task

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
 public class UserService {
-
-    private PasswordValidator passwordValidator = new PasswordValidator();
-
     public void registerUser(User user) {
+        PasswordValidator validator = new PasswordValidator();
         try {
-            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            validator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
         } catch (PasswordValidationException e) {
             System.out.println("Your passwords are incorrect. Try again.");
