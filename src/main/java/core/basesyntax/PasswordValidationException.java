46f02a403d4a6db2c7a08f03fe0d754553919a08
Mirrox-cloud <beznosmaksim0102@gmail.com>
2025-04-20 19:43:02 +0300

    solved task

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,8 @@
 package core.basesyntax;
 
 //write your code here
+public class PasswordValidationException extends Exception {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
