66585c8fb5d3fe96f80c2b04b5b4822cd0dd3031
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-04-28 22:30:10 +0300

    implemented method 'validate' at the class PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    private static final String MESSAGE_EXCEPTION = "Wrong passwords";
+    private static final int MIN_LENGTH_PASSWORD = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        try {
+            if (!password.equals(repeatPassword)) {
+                throw new PasswordValidationException(MESSAGE_EXCEPTION);
+            }
+            if (password.length() < MIN_LENGTH_PASSWORD) {
+                throw new PasswordValidationException(MESSAGE_EXCEPTION);
+            }
+        } catch (RuntimeException e) {
+            throw new PasswordValidationException(MESSAGE_EXCEPTION);
+        }
     }
 }
