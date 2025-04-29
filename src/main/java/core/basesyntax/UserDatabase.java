632a2e49136675318caf3ad6d12892990c55bd3f
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:31:49 +0300

    Update ci.yml

---
+++ src/main/java/core/basesyntax/UserDatabase.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class UserDatabase {
+    public void saveUser(String username, String password) {
+        System.out.println("User " + username + " was saved to database!!!");
+    }
+}
