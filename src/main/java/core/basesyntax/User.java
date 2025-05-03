c801786c885e6f894366792b9a1e74236f60effb
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-03 19:47:48 +0200

    Fix Checkstyle issue: Add newline at the end of User.java

--- src/main/java/core/basesyntax/User.java
+++ src/main/java/core/basesyntax/User.java
@@ -1,57 +1,24 @@
 package core.basesyntax;
 
 public class User {
-    private String username; // New variable
-    private String email;
-    private String password;
-    private String repeatPassword;
+    private String name;
+    private int age;
 
-    public User(String username, String email, String password, String repeatPassword) {
-        this.username = username; // Initialize username
-        this.email = email;
-        this.password = password;
-        this.repeatPassword = repeatPassword;
+    // Getters and setters
+    public String getName() {
+        return name;
     }
 
-    public String getUsername() {
-        return username;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public int getAge() {
+        return age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
-    }
-
-    @Override
-    public String toString() {
-        return "User{"
-                + "username='" + username + '\'' // Include username in toString
-                + ", email='" + email + '\''
-                + ", password='" + password + '\''
-                + ", repeatPassword='" + repeatPassword + '\''
-                + '}';
+    public void setAge(int age) {
+        this.age = age;
     }
 }
+// (Add an empty line here)
