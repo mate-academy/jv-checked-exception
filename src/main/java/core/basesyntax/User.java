94668ef474337e545095a6d934a1d8159edd4d01
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-03 20:14:07 +0200

    Fix Checkstyle: Add newline at end of User.java

---
+++ src/main/java/core/basesyntax/User.java
@@ -0,0 +1,46 @@
+package core.basesyntax;
+
+public class User {
+    private String email;
+    private String password;
+    private String repeatPassword;
+
+    public User(String email, String password, String repeatPassword) {
+        this.email = email;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                '}';
+    }
+}
