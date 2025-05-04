5fbaeb4e9b6e9772918b73cfa611a43f62fd09d4
Dmytro T <dmitriytverdokhlibov@yahoo.com>
2025-05-04 22:49:11 +0200

    feat: implemented validate method and PasswordValidationException class
    - Added validate method for password checking.
    - Created PasswordValidationException class to handle validation errors.
    - Implemented registerUser method.
    
    Current issues:
    - Facing Checkstyle errors, possibly due to incorrect error message formatting.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password,
+                         String repeatPassword) throws PasswordValidationException {
+        if (password != null
+                && password.equals(repeatPassword)
+                && password.length() >= 10) {
+            System.out.println("Password validation successful");
+        } else {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+        }
     }
 }
